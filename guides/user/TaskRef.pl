807046e72e1d653a8505a0ff5668f7f774cafe7a
Mark Calabretta <mcalabre@atnf.csiro.au>
2006-10-06 04:23:19 +0000

    Updated to reflect the new location of the userguide in $MIR/html/userguide/.

--- guides/user/TaskRef.pl
+++ guides/user/TaskRef.pl
@@ -9,7 +9,7 @@ foreach $f ( <node*.html> ){
   ($n) = ($f =~ m{.*node(\d+)\.html});
   $_ = `cat $f`;
   ($title) = m{<TITLE>([^<]*)</TITLE>};
-  while(m{<A\s+([^>]*\s+|)HREF="(\w+)\.html"(\s+[^>]*|)\s*>}g){
+  while (m{<A\s+([^>]*\s+|)HREF="\.\./(\w+)\.html"(\s+[^>]*|)\s*>}g){
     if( -x ($ENV{"MIRBIN"} . "/" . $2)){
       $line = sprintf("%-10s %4d %s",$2,$n,$title);
       push @list,$line;
@@ -30,5 +30,5 @@ foreach $f (@list){
 sub genline{
   my $task; my $n; my $title;
   ($task,$n,$title) = ($_[0] =~m{(\w+)\s+(\d+)\s+(.*)});
-  print "$task <A HREF=node$n.html>$title</A>\n";
+  print "$task <A HREF=userguide/node$n.html>$title</A>\n";
 }
