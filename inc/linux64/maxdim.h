bdf5949897bed46310fd139e1497e4211c4072b5
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-03-29 07:25:35 +0000

    Changed the defaults for MAXBUF and MAXDIM and added copious comments regarding them.

--- inc/linux64/maxdim.h
+++ inc/linux64/maxdim.h
@@ -1,17 +1,25 @@
 c=======================================================================
 c $Id$
 c-----------------------------------------------------------------------
-c     Size of an INTEGER array used to implement a memory heap.  Must be
-c     less than the maximum 4-byte Fortran INTEGER value of 2147483647
-c     which causes expressions of the form (MAXBUF+1)/2 to overflow.
+c     Size of an INTEGER array used to implement a memory heap.  This
+c     array is the sole variable in blank COMMON in Miriad.  Trial-and-
+c     error compilations on an x86-64 system with gcc/g77 show that the
+c     limit on MAXBUF for which most tasks build successfully is
+c     1073741823 (2**30 - 1) which corresponds to 4GiB.  The limit for
+c     which all tasks build successfully is about 200000000, where
+c     unsuccessful links produce messages about truncated relocations.
+c     The default value allocates 128MiB (for normal 4-byte INTEGERs).
       INTEGER   MAXBUF
-      PARAMETER(MAXBUF=4000000)
-c      PARAMETER(MAXBUF=2147483646)
+      PARAMETER(MAXBUF=  33554432)
 
-c     Maximum image dimension.
+c     Maximum image axis length.  Array dimensions are typically a few
+c     times MAXDIM (never MAXDIM**2) so MAXDIM is associated with a much
+c     smaller allocation of static memory than MAXBUF.  Thus the default
+c     value of MAXDIM is quite generous.  Note that, depending on the
+c     algorithm, MAXBUF may also play an important role in determining
+c     the maximum image size that can be handled.
       INTEGER   MAXDIM
-      PARAMETER(MAXDIM=16384)
-c      PARAMETER(MAXDIM=65536)
+      PARAMETER(MAXDIM=32768)
 
 c     Maximum number of antennae (HC=3/6/9/..., WSRT=14, VLA=27).
       INTEGER   MAXANT
