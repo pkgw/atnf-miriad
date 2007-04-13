25fe2128ccba4894199798b73584a2a8bd1f7a1c
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-04-13 01:12:31 +0000

    Initial revision; to match the linux64 version of maxdim.h.

---
+++ inc/linux64/maxdimc.h
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------
+*  maxdimc.h - C include file containing MIRIAD-wide parameters.
+*
+*  Note that MAXBUF defined here is only used by the routines in xyzio.c
+*  which are only used by a handful of tasks.  It is NOT the same as the
+*  MAXBUF defined for Fortran use in maxdim.h and only needs to be large
+*  enough for efficient I/O.  xyzio actually allocates two buffers, one
+*  float and the other int.  The default value of 16MiB each should be
+*  plenty.
+*
+*  The remaining parameters must match those in maxdim.h, or maxnax.h for
+*  MAXNAX.
+*
+*  MAXDIM is only used by the xyio.c routines.
+*
+*  MAXNAX is only used by the xyio.c, xyzio.c and xyziowrap.c routines.
+*
+*  MAXANT and MAXWIN are only used by the uvio.c routines.
+*
+*  MAXBASE, MAXCHAN, and MAXWIDE are not used by anything.
+*
+* $Id$
+*---------------------------------------------------------------------------*/
+/* Maximum buffer size for xyzio (N.B. see comment above). */
+#define MAXBUF  4194304
+
+/* Maximum number of pixels on an image axis. */
+#define MAXDIM  16384
+
+/* Maximum number of image axes. */
+#define MAXNAX  7
+
+/* Maximum number of antennas. */
+#define MAXANT  30
+
+/* Maximum number of baselines. */
+#define MAXBASE ((MAXANT * (MAXANT + 1)) / 2)
+
+/* Maximum number of channels in spectral data. */
+#define MAXCHAN 4097
+
+/* Maximum number of windows in uv data. */
+#define MAXWIN  16
+
+/* Maximum number of wideband channels. */
+#define MAXWIDE 18
