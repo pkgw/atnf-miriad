d20935909ef94355e45e55c563caf203ba675f80
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:25:47 +0000

    Old Miriad archive file. File last modification timestamp: 19970707   Date recorded in the file name: 19981120

--- subs/co.h
+++ subs/co.h
@@ -16,12 +16,10 @@ c
 	double precision cdelt(MAXNAX,MAXOPEN)
 	double precision restfreq(MAXOPEN),vobs(MAXOPEN)
 	double precision epoch(MAXOPEN),obstime(MAXOPEN)
-	double precision llcos(MAXOPEN),llsin(MAXOPEN)
 	character ctype(MAXNAX,MAXOPEN)*16,coproj(MAXOPEN)*3
 	integer cotype(MAXNAX,MAXOPEN)
 	logical cellscal(MAXOPEN)
 c
 	common/cocom/crval,crpix,cdelt,restfreq,vobs,epoch,obstime,
-     *		llcos,llsin,
      *		lus,nalloc,naxis,cotype,ilong,ilat,ifreq,cellscal
 	common/cocomc/ctype,coproj
