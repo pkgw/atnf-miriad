a89d1972240af1851dcff4c5e1445a15f6ffb8b5
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:19:39 +0000

    Old Miriad archive file. File last modification timestamp: 19961129   Date recorded in the file name: 19970219

--- prog/selfcal.h
+++ prog/selfcal.h
@@ -5,13 +5,13 @@ c------------------------------------------
 	include 'mem.h'
 	integer maxHash,minSol
 	parameter(maxHash=30000,minSol=200)
-	integer nants,nBl,nbad,nbstok,maxSol,nsols,TotVis,minants
+	integer nants,nBl,nbad,maxSol,nsols,TotVis,minants
 	integer nHash,Hash(maxHash),Indx(maxHash)
 	integer pSumVM,pSumVV,pSumMM,pWeight,pCount,pGains,prTime
         integer pstptim,pstrtim
 	logical first
 	double precision time0
-	common/selfcom/time0,nants,nBl,nbad,nbstok,nsols,maxSol,
+	common/selfcom/time0,nants,nBl,nbad,nsols,maxSol,
      *	  TotVis,minants,nHash,Hash,Indx,pSumVm,pSumVV,pSumMM,pWeight,
      *	  pCount,pGains,prTime,pstptim,pstrtim,first
 
