2fcbac748b30825c8c383832f2c8ff2b403404b7
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:13:49 +0000

    Old Miriad archive file. File last modification timestamp: 19950119   Date recorded in the file name: 19950328

--- prog/atxy.h
+++ prog/atxy.h
@@ -2,5 +2,5 @@
 	parameter(ATANT=6,MAXTIME=10000,MAXSOL=2*MAXTIME)
 	integer idx(MAXTIME),nidx(MAXTIME),nsol,ntime
 	double precision time(MAXTIME),freq(MAXSOL)
-	complex xyphase(ATANT,MAXSOL)
-	common/atxycom/time,freq,xyphase,idx,nidx,nsol,ntime
+	complex xyphase(ATANT,MAXSOL),axyphase
+	common/atxycom/time,freq,xyphase,axyphase,idx,nidx,nsol,ntime
