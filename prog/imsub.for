e54fd5b15cb23c175b868cb084ef3cc5fa033980
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:17:49 +0000

    Old Miriad archive file. File last modification timestamp: 19990724 From an incremental backup dated: 19990805

--- prog/imsub.for
+++ prog/imsub.for
@@ -39,9 +39,11 @@ c    nebk 10jan96  Change crpix and cdelt to double
 c    rjs  02jul97  cellscale change.
 c    rjs  18jul97  Correct handling of flags when incr != 1.
 c    rjs  23jul97  added pbtype.
+c    rjs  25nov98  added llrot.
+c    rjs  23jul99  Correct copying of pbtype keyword.
 c------------------------------------------------------------------------
 	character version*(*)
-	parameter(version='Imsub: version 1.0 18-Jul-97' )
+	parameter(version='Imsub: version 1.0 23-Jul-99' )
 c
 	integer maxboxes
 	parameter(maxboxes=2048)
@@ -64,7 +66,7 @@ c
 c  Header keywords.
 c
 	integer nkeys
-	parameter(nkeys=36)
+	parameter(nkeys=37)
 	character keyw(nkeys)*8
 	data keyw/   'bmaj    ','bmin    ','bpa     ','bunit   ',
      *	  'crval1  ','crval2  ','crval3  ','crval4  ','crval5  ',
@@ -73,7 +75,7 @@ c
      *	  'object  ','observer','obsra   ','obsdec  ','pbfwhm  ',
      *	  'restfreq','telescop','vobs    ','btype   ','rms     ',
      *	  'ltype   ','lstart  ','lwidth  ','lstep   ','mostable',
-     *	  'cellscal','pbfwhm  '/
+     *	  'cellscal','pbtype  ','llrot   '/
 c
 c  Get the input parameters.
 c
