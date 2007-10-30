efe6c18b5cde6183f09533fa87ae32e185b9e18f
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:16:36 +0000

    Old Miriad archive file. File last modification timestamp: 19961008   Date recorded in the file name: 19961011

--- prog/histo.for
+++ prog/histo.for
@@ -33,6 +33,7 @@ c     1may92 rjs   Increase maxruns. Various trivial other mods.
 c     2mar93 rjs   Use maxnax.h.
 c    19nov93 rjs   Better summary of flux or sum.
 c    31jan96 nebk  More grace when no valid pixels in region.  
+c    08oct96 nebk  Make accumulation sums double precision
 c------------------------------------------------------------------------
 	include 'maxdim.h'
 	include 'maxnax.h'
@@ -40,7 +41,7 @@ c------------------------------------------------------------------------
 	character version*(*)
 	parameter(nbindef=16,nbinmax=40,maxboxes=2048)
 	parameter(maxruns=80000)
-	parameter(version = 'version 1.0 31-Jan-96' )
+	parameter(version = 'version 08-Oct-96' )
 c
 	character file*40,asterisk*30,line*72,coord*64,bunit*32
 	integer nsize(MAXNAX),plane(MAXNAX),maxv(MAXNAX),minv(MAXNAX)
@@ -48,8 +49,9 @@ c
 	integer i,j,k,under,over,bin(nbinmax),maxbin
 	integer naxis,indx,lun,npoints,length,nbin
 	integer boxes(maxboxes),runs(3,maxruns),nruns
-	real dat(maxdim),rmax,rmin,sum,sum2,av,rms,blo,bhi,x,xinc,r
+	real dat(maxdim),rmax,rmin,blo,bhi,x,xinc,r
 	real bscale,bmaj,bmin,barea,cdelt1,cdelt2
+        double precision sum,sum2,av,rms
 	logical first, done, newmin, newmax, norange
 c
 c  Externals.
