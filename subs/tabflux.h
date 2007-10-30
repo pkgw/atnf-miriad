17198f7e10fb475191f8cc1ec5adc25c80dafda6
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:28:06 +0000

    Old Miriad archive file. File last modification timestamp: 19941116   Date recorded in the file name: 19951103

--- subs/tabflux.h
+++ subs/tabflux.h
@@ -2,11 +2,14 @@
       parameter (NTABLE=2000)
 c
       character TSOURCE(NTABLE)*40
+      character ROOT(NTABLE)*40
+      character ALIAS(NTABLE)*40
       integer NTAB, TINDEX(NTABLE)
+      integer NALIASES
       real TFREQ(NTABLE), TFLUX(NTABLE), TRMS(NTABLE)
       double precision TDATE(NTABLE)
 c
-      common / TCOMI /NTAB, TINDEX
+      common / TCOMI /NTAB, TINDEX, NALIASES
       common / TCOMR /TFREQ, TFLUX, TRMS
       common / TCOMD /TDATE
-      common / TCOMS /TSOURCE
+      common / TCOMS /TSOURCE, ROOT, ALIAS
