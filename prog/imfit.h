28aad226edd0de58a3e36a4ac643f258d0b55313
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:16:58 +0000

    Old Miriad archive file. File last modification timestamp: 19940930   Date recorded in the file name: 19950407

--- prog/imfit.h
+++ prog/imfit.h
@@ -1,5 +1,5 @@
-	integer LEVEL,GAUSSIAN,DISK
-	parameter(LEVEL=1,GAUSSIAN=2,DISK=3)
+	integer LEVEL,GAUSSIAN,DISK,POINT,BEAM
+	parameter(LEVEL=1,GAUSSIAN=2,DISK=3,POINT=4,BEAM=5)
 	integer MAXSRC,MAXDATA
 	parameter(MAXSRC=10,MAXDATA=100000)
 	integer nsrc,ndata
@@ -10,6 +10,9 @@
 	integer x(MAXDATA),y(MAXDATA)
 	logical vflux(MAXSRC),vl0(MAXSRC),vm0(MAXSRC)
 	logical vfwhm1(MAXSRC),vfwhm2(MAXSRC),vpa(MAXSRC),circ(MAXSRC)
+	real    sflux(MAXSRC),sl0(MAXSRC),sm0(MAXSRC)
+	real    sfwhm1(MAXSRC),sfwhm2(MAXSRC),spa(MAXSRC)
 c
-	common/ImFit/vflux,vl0,vm0,vfwhm1,vfwhm2,vpa,circ,
-     *	  nsrc,srctype,ndata,x,y,l0,m0,fwhm1,fwhm2,pa,flux,Data
+	common/JmFit/vflux,vl0,vm0,vfwhm1,vfwhm2,vpa,circ,
+     *	  nsrc,srctype,ndata,x,y,l0,m0,fwhm1,fwhm2,pa,flux,Data,
+     *		     sflux,sl0,sm0,sfwhm1,sfwhm2,spa
