ecb60af03af12c47d31be5021c8be2209ba52280
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:26:14 +0000

    Old Miriad archive file. File last modification timestamp: 19991012 From an incremental backup dated: 19991104

--- subs/getxy.for
+++ subs/getxy.for
@@ -3,11 +3,11 @@ c* GetXY -- Return information on the XY phase of a dataset.
 c& rjs
 c: utilities
 c+
-	subroutine GetXY(tno,xyphase,sd,count,mxant,nants)
+	subroutine GetXY(tno,fac,xyphase,count,mxant,nants)
 c
 	implicit none
 	integer tno,mxant,nants,count(mxant)
-	real xyphase(mxant),sd(mxant)
+	real xyphase(mxant),fac
 c
 c  This determines the average XY phase, and its standard deviation, of
 c  the gains of a visibility data-set. The information is derived
@@ -17,8 +17,8 @@ c  Input:
 c    tno	Handle of the input visibility data-set.
 c    mxant	Max number of antennae.
 c  Output:
+c    fac        RMS gain amplitude.
 c    xyphase	Mean xyphase, in radians.
-c    sd		Standard deviation of the xyphase, in radians.
 c    count	Number of gains used in the average. This can be 0!
 c    nants	Number of antenna found. This will be zero of the xyphase
 c		could not be determined at all.
@@ -26,8 +26,8 @@ c--
 c------------------------------------------------------------------------
 	include 'maxdim.h'
 	include 'mirconst.h'
-	integer item,iostat,ngains,nfeeds,ntau,nsols,i,j,k,off
-	real theta
+	integer item,iostat,ngains,nfeeds,ntau,nsols,i,j,k,off,ntot
+	real theta,rms
 	complex gx,gy,gains(3*MAXANT)
 c
 c  Determine the number of gains, antennae, etc.
@@ -49,10 +49,11 @@ c
 c
 c  All looks OK. Initialise the counters.
 c
+	rms = 0
+	ntot = 0
 	do i=1,nants
 	  count(i) = 0
 	  xyphase(i) = 0
-	  sd(i) = 0
 	enddo
 c
 c  Read through the gains, compute the XY phase at each antenna, and
@@ -71,13 +72,15 @@ c
 	    gy = gains(k+1)
 	    if(abs(real(gx))+abs(aimag(gx)).gt.0.and.
      *	       abs(real(gy))+abs(aimag(gy)).gt.0)then
+	      rms = rms + real(gx)**2 + aimag(gx)**2 +
+     *			  real(gy)**2 + aimag(gy)**2
+	      ntot = ntot + 2
 	      gx = gx / gy
 	      theta = atan2(aimag(gx),real(gx))
 	      if(count(i).gt.0)theta = theta +
      *		  2*pi*nint((xyphase(i)/count(i) - theta)/(2*pi))
 	      count(i) = count(i) + 1
 	      xyphase(i) = xyphase(i) + theta
-	      sd(i) = sd(i) + theta*theta
 	    endif
 	    k = k + nfeeds + ntau
 	  enddo
@@ -91,9 +94,9 @@ c
 	  if(count(i).gt.0)then
 	    theta = xyphase(i) / count(i)
 	    xyphase(i) = theta
-	    sd(i) = sqrt(abs(sd(i)/count(i) - theta*theta))
 	  endif
 	enddo
+	if(ntot.eq.0)fac = sqrt(rms/ntot)
 c
 	end
 	
