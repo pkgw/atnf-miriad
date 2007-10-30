c441ade580175e812aa793b197768314dbf385af
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:16:42 +0000

    Old Miriad archive file. File last modification timestamp: 19951127   Date recorded in the file name: 19960816

--- prog/imblr.for
+++ prog/imblr.for
@@ -18,10 +18,9 @@ c--
 c
 c  History:
 c    nebk 27Nov95  Original version
-c    nebk 16aug96  Tell user some numbers
-c    nebk 13sep96  Tell them some more
-c    rjs  12oct99  Do not write out gflags.
-c    rjs  08may00  Change incorrect call to keyf to keya.
+c
+c  Notes:
+c    Uses cgsubs.for
 c
 c-----------------------------------------------------------------------
       implicit none
@@ -30,12 +29,14 @@ c
       include 'maxnax.h'
 c
       character version*20
-      parameter (version = 'Version 12-Oct-99')
+      parameter (version = 'Version 27-Nov-95')
 c
       integer size(maxnax), lin, lout, i, j, k, naxis
-      real dmm(2), val, data(maxdim), npix, npix2
-      logical flags(maxdim),first
-      character in*64, out*64, line*132
+      real dmm(2), val, data(maxdim)
+      logical gflags(maxdim), flags(maxdim)
+      character in*64, out*64
+      data gflags /maxdim*.true./
+      data dmm /1.0e30, -1.0e30/
 c-----------------------------------------------------------------------
       call output ('ImBLR: '//version)
 c
@@ -43,7 +44,7 @@ c Get user inputs
 c
       call keyini
       call keyf ('in', in, ' ')
-      call keya ('out', out, ' ')
+      call keyf ('out', out, ' ')
       call keyr ('value', val, 0.0)
       call keyfin
 c
@@ -68,44 +69,25 @@ c
 c
 c Loop over input image
 c
-      first = .true.
-      npix2 = 0.0
       do k = 1, size(3)
-        npix = 0.0
         call xysetpl (lin, 1, k)
         call xysetpl (lout, 1, k)
 c
         do j = 1, size(2)
           call xyread (lin, j, data)
-	  if(first)then
-	    dmm(1) = data(1)
-	    dmm(2) = dmm(1)
-	    first = .false.
-	  endif
           call xyflgrd (lin, j, flags)
 c
           do i = 1, size(1)
-            if (.not.flags(i)) then
-              data(i) = val
-              npix = npix + 1
-            end if
+            if (.not.flags(i)) data(i) = val
             dmm(1) = min(dmm(1),data(i))
             dmm(2) = max(dmm(2),data(i))
           end do
 c
           call xywrite (lout, j, data)
+          call xyflgwr (lout, j, gflags)
         end do
-c
-        if (npix.gt.0.0) then
-          write (line, 10) k, npix
-10        format ('Plane ', i3, ' : replaced ', f8.0, ' blanks')
-          call output (line)  
-          npix2 = npix2 + npix
-        end if
       end do
 c      
-      if (npix2.le.0.0) call output ('There were no blanks')
-c   
       call wrhdr (lout, 'datamax', dmm(2))
       call wrhdr (lout, 'datamin', dmm(1))
 c
