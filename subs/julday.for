067f790d3b6555e2dc010db88d6fbc0dfb63f1f4
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:26:34 +0000

    Old Miriad archive file. File last modification timestamp: 19970911   Date recorded in the file name: 19970912

--- subs/julday.for
+++ subs/julday.for
@@ -38,7 +38,8 @@ c     jm    14mar94  Added check for blank input strings; removed tabs.
 c    rjs    10oct94  Round date in julfdate.
 c     jm    07jun96  To avoid integer overflow in dayjul, I added code
 c                    to strip off trailing 0s beyond the decimal point.
-c    rjs    10sep97  Added "djulian" function.
+c    rjs    10sep97  Added "caljul" function.
+c    rjs    10sep97  Added functionality for IAU 'T' format.
 c***********************************************************************
 c* JulDay -- Format a Julian day into a conventional calendar day.
 c& jm
@@ -57,7 +58,7 @@ c  The day is assumed to begin at 0 hours UT.
 c
 c  Input:
 c    julian      Julian date.
-c    form        Output selection flag (Must be either 'H' or 'D').
+c    form        Output selection flag (Must be 'H', 'D', or 'T').
 c
 c  Output:
 c    calday      (Gregorian) Calendar day (UT time).
@@ -65,6 +66,10 @@ c                The output if form = 'D' is like:
 c                       'yymmmdd.dd'
 c                The output if form = 'H' is like:
 c                       'yymmmdd:hh:mm:ss.s'
+c                The output if form = 'T' is like:
+c                       'ccyy-mm-ddThh:mm:ss.s'
+c                where cc is used to denote the century and the
+c                T is literal.
 c
 c--
 c-----------------------------------------------------------------------
@@ -76,9 +81,9 @@ c     parameter(fudge=0.5/(24.0*60.0*60.0*10.0))
 c
       double precision f
       integer z,a,b,c,d,e,alpha,month,year,day,hr,minute,sec
-      integer dsec, decday,nchar
+      integer dsec, decday, nchar, century
       character months(12)*3
-      character outform*80, outstr*20
+      character outform*80, outstr*25
 c
       integer Len1
 c
@@ -115,6 +120,7 @@ c
       else
         year = c - 4715
       endif
+      century = year / 100
       year = mod(year, 100)
 c
       nchar = Len1(form)
@@ -135,6 +141,12 @@ c
      *    hr, minute, sec, dsec
    20   format(i2.2, a, i2.2, ':', i2.2, ':', i2.2, ':', i2.2, '.', i1)
         nchar = 18
+      else if (index(outform(1:nchar), 'T') .ne. 0) then
+        write (outstr, 30) century, year, month, day,
+     *    hr, minute, sec, dsec
+   30   format(i2.2, i2.2, '-', i2.2, '-', i2.2, 'T',
+     *    i2.2, ':', i2.2, ':', i2.2, '.', i1)
+        nchar = 21
       else
         outstr = ' '
         outform = PROG // 'Incorrect format selected ' // form(1:nchar)
@@ -166,35 +178,38 @@ c
 c  Input:
 c    calday   (Gregorian) Calendar day (UT time).
 c             The input must be either of the form:
-c                     ``yymmmdd.dd''
+c                     `yymmmdd.dd'
 c             or:
-c                     ``[yymmmdd:][hh[:mm[:ss.s]]]''
+c                     `[yymmmdd:][hh[:mm[:ss.s]]]'
+c             or:
+c                     `ccyy-mm-ddT[hh[:mm[:ss.s]]]'
 c             where the brackets delimit optional values.
 c
-c             The presence (or lack of) a period (``.'') immediately
-c             after the ``yymmmdd'' string determines which format to
-c             decode.  If the period is present, the entire string
-c             is expected to be present (ie. ``yymmmdd.dd'').  If
-c             the second format is entered, the presence (or lack of)
-c             the ``yymmmdd'' string determines the range of the
-c             output value.  Without the ``yymmmdd'' string, the
-c             output julian date will be in the range [0, 1].  If
-c             the ``yymmmdd'' portion of the string is present, the
-c             full julian date is computed.
+c             If the dash and the literal `T' are present, then the
+c             third format is assumed and decoded.  The presence (or
+c             lack of) a period (`.') immediately after the `yymmmdd'
+c             string determines which of the first two formats to
+c             decode.  If the period is present, the entire string is
+c             expected to be present (ie. `yymmmdd.dd').  If the
+c             second format is entered, the presence (or lack of)
+c             the `yymmmdd' string determines the range of the output
+c             value.  Without the `yymmmdd' string, the output julian
+c             date will be in the range [0, 1].  If the `yymmmdd'
+c             portion of the string is present, the full julian date
+c             is computed.
 c
-c             When the input string is of the second type, it
-c             may be abbreviated with the missing entries internally
-c             filled with a value of zero.  With the ``yymmmdd''
-c             string included, the year, month, and day values
-c             are decoded and then the string is decoded for the
-c             time value.  Without the ``yymmmdd'' string or after
-c             it has been decoded, the routine will interpret the
-c             string up to the next colon as ``hh''; text after
-c             that colon as ``mm''; and any text after the next
-c             colon as ``ss.s''.  Hence, an input string of ``3''
-c             will be interpreted as if it were entered as
-c             ``03:00:00.0''.  Also, if the input string was
-c             ``3:10'', it will be interpreted as ``03:10:00.0''.
+c             When the input string is of the second type, it may
+c             be abbreviated with the missing entries internally
+c             filled with a value of zero.  With the `yymmmdd' string
+c             included, the year, month, and day values are decoded
+c             and then the string is decoded for the time value.
+c             Without the `yymmmdd' string or after it has been
+c             decoded, the routine will interpret the string up to
+c             the next colon as `hh'; text after that colon as `mm';
+c             and any text after the next colon as `ss.s'.  Hence,
+c             an input string of `3' will be interpreted as if it were
+c             entered as `03:00:00.0'.  Also, if the input string was
+c             `3:10', it will be interpreted as `03:10:00.0'.
 c
 c  Output:
 c    julian   Julian date (may be an offset date; see ``calday'' above).
@@ -205,16 +220,17 @@ c-----------------------------------------------------------------------
 c
       double precision f
       integer j,a,b,z
-      integer ncolon, nchar, value, zero
+      integer bsave
+      integer ncolon, nchar, ndash, value, zero
       integer month, year, day, hr, minute, sec, dsec, decday
-      integer mmonth(12),days(12)
+      integer mmonth(12), days(12)
       character months(12)*3, mon*3
       character errmsg*80
       logical alpha, fset, ok
 c
       integer Len1,binsrcha
       logical Isalphaf
-      double precision djulian
+      double precision caljul
 c
       data months/'APR','AUG','DEC','FEB','JAN','JUL',
      *            'JUN','MAR','MAY','NOV','OCT','SEP'/
@@ -241,18 +257,32 @@ c
       endif
       b = index(calday(a:z), '.')
       if (b .eq. 0) b = z + 1
+      ndash = index(calday(a:z), '-')
+      if (ndash .gt. 0) then
+        bsave = b
+        b = index(calday(a:z), 'T')
+        if (b .eq. 0) b = z + 1
+      endif
       ncolon = 0
+      ndash = 0
       alpha = .FALSE.
       do j = a, (b - 1)
         if (calday(j:j) .eq. ':') then
           if (ncolon .eq. 0) value = j
           ncolon = ncolon + 1
+        else if (calday(j:j) .eq. '-') then
+          ndash = ndash + 1
         endif
         if (ncolon .eq. 0) then
           if (Isalphaf(calday(j:j))) alpha = .TRUE.
         endif
       enddo
       if (ncolon .ne. 0) ncolon = value
+      if ((ndash .ne. 0) .and. (ndash .ne. 2)) then
+        errmsg = PROG // 'Date T-format incorrect ' // calday(1:z)
+        nchar = Len1(errmsg)
+        call Bug('f', errmsg(1:nchar))
+      endif
 c
 c  Initialize all integers in case they are not present in the string.
 c
@@ -273,35 +303,65 @@ c
 c  Decode formatted string.
 c  First get the yymmmdd string and, if in 'D' format, get the decimal date.
 c
-      if (alpha) then
-        value = 0
-        nchar = b
-        if (ncolon .gt. 0) nchar = min(nchar, ncolon)
-        do while ((ok) .and. (a .lt. nchar))
-          if (Isalphaf(calday(a:a))) then
-            if (mon .ne. ' '.or.a.eq.1)then
-              ok = .FALSE.
-            else
+      if (alpha .or. (ndash .eq. 2)) then
+        if (ndash .eq. 2) then
+          value = 0
+          nchar = b
+          b = bsave
+          do while ((ok) .and. (a .lt. nchar))
+            if (calday(a:a) .eq. '-') then
               value = value + 1
-              mon = calday(a:a+2)
-              a = a + 3
-            endif
-          else
-            j = ichar(calday(a:a)) - zero
-            ok = ((j .ge. 0) .and. (j .le. 9))
-            if (value .eq. 0) then
-              year = (year * 10) + j
-            else if (value .eq. 1) then
-              day = (day * 10) + j
+            else
+              j = ichar(calday(a:a)) - zero
+              ok = ((j .ge. 0) .and. (j .le. 9))
+              if (value .eq. 0) then
+                year = (year * 10) + j
+              else if (value .eq. 1) then
+                month = (month * 10) + j
+              else if (value .eq. 2) then
+                day = (day * 10) + j
+              else
+                ok = .FALSE.
+              endif
             endif
             a = a + 1
+          enddo
+          if (ok .and. (a .lt. b)) then
+            ok = (calday(a:a) .eq. 'T')
+            a = a + 1
           endif
-        enddo
-        a = a + 1
+        else if (alpha) then
+          value = 0
+          nchar = b
+          if (ncolon .gt. 0) nchar = min(nchar, ncolon)
+          do while ((ok) .and. (a .lt. nchar))
+            if (Isalphaf(calday(a:a))) then
+              if (mon .ne. ' '.or.a.eq.1)then
+                ok = .FALSE.
+              else
+                value = value + 1
+                mon = calday(a:a+2)
+                a = a + 3
+              endif
+            else
+              j = ichar(calday(a:a)) - zero
+              ok = ((j .ge. 0) .and. (j .le. 9))
+              if (value .eq. 0) then
+                year = (year * 10) + j
+              else if (value .eq. 1) then
+                day = (day * 10) + j
+              else
+                ok = .FALSE.
+              endif
+              a = a + 1
+            endif
+          enddo
+          a = a + 1
+        endif
 c
 c  Check that all is OK.
 c
-        if(.not.ok)then
+        if (.not. ok) then
           errmsg = PROG // 'Incorrectly formatted date ' // calday(1:z)
           nchar = Len1(errmsg)
           call Bug('f', errmsg(1:nchar))
@@ -309,19 +369,21 @@ c
 c
 c  Check the month and the day of the month. Get them into a standard form.
 c
-        if(year.gt.99)then
-          errmsg = PROG // 'Bad year format: ' // calday(1:z)
-          call Bug('f', errmsg)
+        if (alpha) then
+          if(year.gt.99)then
+            errmsg = PROG // 'Bad year format: ' // calday(1:z)
+            call Bug('f', errmsg)
+          endif
+          year = year + 1900
+          if (year .lt. 1950) year = year + 100
+          call Ucase(mon)
+          month = binsrcha(mon,months,12)
+          if(month.eq.0)then
+            errmsg = PROG // 'Unrecognised month: ' // calday(1:z)
+            call Bug('f', errmsg)
+          endif
+          month = mmonth(month)
         endif
-        year = year + 1900
-        if (year .lt. 1950) year = year + 100
-        call Ucase(mon)
-        month = binsrcha(mon,months,12)
-        if(month.eq.0)then
-          errmsg = PROG // 'Unrecognised month: ' // calday(1:z)
-          call Bug('f', errmsg)
-        endif
-        month = mmonth(month)
         if(day.gt.days(month)) then
           errmsg = PROG // 'Bad number of days in month: '// calday(1:z)
           call Bug('f', errmsg)
@@ -329,7 +391,7 @@ c
 c
 c  Process something in 'D' format.
 c
-        if ((ncolon .eq. 0) .and. (b .lt. z)) then
+        if ((ndash .eq. 0) .and. (ncolon .eq. 0) .and. (b .lt. z)) then
 c
 c  Get decimal day value.
 c
@@ -396,6 +458,11 @@ c
             z = z - 1
           enddo
           j = b + 1
+c
+c  Add a sanity check for too many digits in fractional day.
+c
+          if (z .gt. (j + 6)) z = j + 6
+c
           nchar = 0
           do while ((ok) .and. (j .le. z))
             value = ichar(calday(j:j)) - zero
@@ -410,7 +477,7 @@ c
           if ((ok) .and. (dsec .gt. 0)) then
 C--old      j = 1 + (log(real(dsec)) / log(10.0))
             j = nchar
-            f = dsec / (3600.0 * 10**j)
+            f = dsec / (3600.0D0 * 10**j)
           endif
         endif
       endif
@@ -426,8 +493,8 @@ c
 c  Add in the fractional day material if it has not already been done.
 c
       if (.not. fset) then
-        f = f + (hr + (minute / 60.0) + (sec / 3600.0))
-        f = f / 24.0
+        f = f + (hr + (minute / 60.0D0) + (sec / 3600.0D0))
+        f = f / 24.0D0
       endif
       f = f + day
 c
@@ -436,101 +503,128 @@ c  then add the year and month information.  If there was no month
 c  present (alpha = FALSE), then the time is a fraction of a day.
 c
       if (alpha) then
-	julian = djulian(year,month,f)
+        julian = caljul(year, month, f)
       else
         julian = f
       endif
 c
       end
 c************************************************************************
-	double precision function djulian(yy,mm,dd)
+      double precision function caljul(yy, mm, dd)
 c
-	implicit none
-	integer yy,mm
-	double precision dd
+      implicit none
+      integer yy, mm
+      double precision dd
 c
-c  Compute Julian day.
+c  Compute the Julian day based on input year, month, and decimal day.
 c
 c  Inputs:
-c    yy		Year (e.g. 1990)
-c    mm		Month (1-12).
-c    dd		Day of the month, and fraction of a day (1-31.99999...).
+c    yy         Year (e.g. 1990).
+c    mm         Month (1-12).
+c    dd         Day of the month, and fraction of a day (1-31.99999...).
 c------------------------------------------------------------------------
-	integer GREG
-c	parameter(GREG=15+31*(10+12*1582))
-	parameter(GREG=588829)
+      integer GREG
+c     parameter(GREG=15+31*(10+12*1582))
+      parameter(GREG=588829)
 c
-	integer a,z,yy1,mm1
+      integer a, z, yy1, mm1
 c
-	z = dd + 31*(mm + 12*yy)
-	if(mm.gt.2)then
-	  mm1 = mm + 1
-	  yy1 = yy
-	else
-	  yy1 = yy - 1
-	  mm1 = mm + 13
-	endif
-	djulian = 1720994.5d0 + int(365.25*yy1) + int(30.6001*mm1) + dd
-	if(z.ge.GREG)then
-	  a = int(0.01*yy1)
-	  djulian = djulian + 2 - a + int(0.25*a)
-	endif
+      z = dd + (31 * (mm + (12 * yy)))
+      if (mm .gt. 2) then
+        mm1 = mm + 1
+        yy1 = yy
+      else
+        yy1 = yy - 1
+        mm1 = mm + 13
+      endif
+      caljul = 1720994.5D0 + int(365.25*yy1) + int(30.6001*mm1) + dd
+      if (z .ge. GREG) then
+        a = int(0.01 * yy1)
+        caljul = caljul + 2 - a + int(0.25 * a)
+      endif
 c
-	end
+      end
 c************************************************************************
 c* FDateJul -- Convert from 'dd/mm/yy' format into a Julian day.
 c& jm
 c: Julian-day, date, utilities
 c+
-      subroutine FDateJul(date,julian)
+      subroutine FDateJul(date, julian)
 c
       implicit none
       double precision julian
       character date*(*)
 c
-c  Convert a date, given as a 'dd/mm/yy' string into a Julian day.
+c  Convert a date given as a 'dd/mm/yy' or 'ccyy-mm-dd' string into
+c  a Julian day.
 c
 c  Input:
-c    date       The date, in 'dd/mm/yy' form.
+c    date       The date, in either 'dd/mm/yy' or 'ccyy-mm-dd' form.
 c
 c  Output:
 c    julian     Julian date.
 c--
 c-----------------------------------------------------------------------
-      integer array(3),i,k
+      integer i, k, z
+      integer array(3)
+      logical dash, slash
 c
-      double precision djulian
+      double precision caljul
 c
 c  Decode the string.
 c
       array(1) = 0
       k = 1
-      do i=1,len(date)
+      z = len(date)
+      dash = (index(date(1:z), '-') .ne. 0)
+      slash = (index(date(1:z), '/') .ne. 0)
+      if(dash.eqv.slash)call bug('f', 'Badly formatted dd/mm/yy string')
+c
+      do i = 1, z
         if(date(i:i).ge.'0'.and.date(i:i).le.'9')then
           array(k) = 10*array(k) + ichar(date(i:i)) - ichar('0')
-        else if(date(i:i).eq.'/')then
+        else if (slash .and. (date(i:i).eq.'/')) then
           k = k + 1
           if(k.gt.3)call bug('f','Badly formatted dd/mm/yy string')
+          array(k) = 0
+        else if (dash .and. (date(i:i).eq.'-')) then
+          k = k + 1
+          if(k.gt.3)call bug('f','Badly formatted ccyy-mm-dd string')
           array(k) = 0
         else if(date(i:i).ne.' ')then
           call bug('f','Badly formatted dd/mm/yy string')
         endif
       enddo
       if(k.ne.3)call bug('f','Badly formatted dd/mm/yy string')
-      if(array(1).le.0.or.array(1).gt.31.or.
-     *   array(2).le.0.or.array(2).gt.12.or.
-     *   array(3).lt.0.or.array(3).gt.99)
-     *  call bug('f','Illegal dd/mm/yy string')
 c
-      julian = djulian(19+array(3),array(2),dble(array(1)))
+      if (slash) then
+        if(array(1).le.0.or.array(1).gt.31.or.
+     *     array(2).le.0.or.array(2).gt.12.or.
+     *     array(3).lt.0.or.array(3).gt.99)
+     *    call bug('f','Illegal dd/mm/yy string')
 c
+        k = array(3) + 1900
+        if (k .lt. 1950) k = k + 100
+        z = array(1)
+      else
+        if(array(1).le.0.or.
+     *     array(2).le.0.or.array(2).gt.12.or.
+     *     array(3).lt.0.or.array(3).gt.31)
+     *    call bug('f','Illegal ccyy-mm-dd string')
+        k = array(1)
+        z = array(3)
+      endif
+c
+      julian = caljul(k, array(2), dble(z))
+c
+      return
       end
 c************************************************************************
 c* JulFDate -- Convert from Julian day into 'dd/mm/yy' format.
 c& jm
 c: Julian-day, date, utilities
 c+
-      subroutine JulFDate(jday,date)
+      subroutine JulFDate(jday, date)
 c
       double precision jday
       character date*(*)
@@ -595,7 +689,7 @@ c-----------------------------------------------------------------------
       integer year, hr, minute, sec, month
       double precision day
 c
-      double precision djulian	
+      double precision caljul
 c
 c  Get the current date.
 c
@@ -608,7 +702,9 @@ c
       hr =     jarray(1)
       minute = jarray(2)
       sec =    jarray(3)
-      day = day + (hr + (minute + sec/60.d0)/60.d0)/24.d0
-      julian = djulian(year,month,day)
+      day = day + ((hr + (minute + (sec / 60.0D0)) / 60.0D0) / 24.0D0)
+      julian = caljul(year, month, day)
 c
+      return
       end
+
