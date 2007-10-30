bc54916ec20dc7f9f6b995b557b0a9b99fe9b7e1
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:18:37 +0000

    Old Miriad archive file. File last modification timestamp: 19970723   Date recorded in the file name: 19970801

--- prog/mosmem.for
+++ prog/mosmem.for
@@ -86,9 +86,12 @@ c    rjs  24nov95  Default default image is now proportional to the gain.
 c    rjs  29Feb96  Call xyflush after each plane.
 c    mwp  27May97  Allow flux estimates for each plane.
 c    rjs  21jun97  Tidies up above change.
+c    rjs  24jun97  Correct call to alignini
+c    rjs  02jul97  cellscal change.
+c    rjs  23jul97  Add pbtype.
 c------------------------------------------------------------------------
 	character version*(*)
-	parameter(version='MosMem: version 1.0 21-Jun-97')
+	parameter(version='MosMem: version 1.0 24-Jun-97')
 	include 'maxdim.h'
 	include 'maxnax.h'
 	include 'mem.h'
@@ -216,7 +219,7 @@ c  output.
 c
 	if(ModelNam.ne.' ')then
 	  call xyopen(lModel,ModelNam,'old',3,nModel)
-	  call AlignIni(lModel,lMap,nModel(1),nModel(2),nModel(3),
+	  call AlignIni(lModel,lMap,nMap(1),nMap(2),nMap(3),
      *						xmoff,ymoff,zmoff)
 	endif
 c
@@ -230,7 +233,7 @@ c  output.
 c
 	if(DefNam.ne.' ')then
 	  call xyopen(lDef,DefNam,'old',3,nDef)
-	  call AlignIni(lDef,lMap,nDef(1),nDef(2),nDef(3),
+	  call AlignIni(lDef,lMap,nMap(1),nMap(2),nMap(3),
      *						xdoff,ydoff,zdoff)
 	endif
 c
@@ -963,7 +966,7 @@ c------------------------------------------------------------------------
 	real crpix
 	character line*72,txtblc*32,txttrc*32,num*2
 	integer nkeys
-	parameter(nkeys=15)
+	parameter(nkeys=17)
 	character keyw(nkeys)*8
 c
 c  Externals.
@@ -971,9 +974,9 @@ c
 	character itoaf*8
 c
 	data keyw/   'obstime ','epoch   ','history ','lstart  ',
-     *	  'lstep   ','ltype   ','lwidth  ','object  ','pbfwhm  ',
+     *	  'lstep   ','ltype   ','lwidth  ','object  ',
      *	  'observer','telescop','restfreq','vobs    ','btype   ',
-     *	  'mostable'/
+     *	  'mostable','cellscal','pbtype  ','pbfwhm  '/
 c
 c  Fill in some parameters that will have changed between the input
 c  and output.
