fc4bf4b9dd5f7d49a0b3625ad0014becd10738b1
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:26:39 +0000

    Old Miriad archive file. File last modification timestamp: 20020627   Date recorded in the file name: 20021211

--- subs/key.h
+++ subs/key.h
@@ -1,7 +1,7 @@
 	integer maxlen,maxkeys
-	parameter(maxlen=7000,maxkeys=32)
-	character pbuf*(maxlen),keys(maxkeys)*8,argv0*32
+	parameter(maxlen=10000,maxkeys=32)
+	character pbuf*(maxlen),keys(maxkeys)*8
 	integer lu(maxkeys),k1(maxkeys),k2(maxkeys),nkeys
 	logical expanded(maxkeys),qhelp
-	common/keycommc/pbuf,argv0,keys
+	common/keycommc/pbuf,keys
 	common/keycomm/ lu,k1,k2,expanded,nkeys,qhelp
