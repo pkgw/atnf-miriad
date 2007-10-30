7092999cda1c7397b992f56147ca8ebcbf785aba
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:27:41 +0000

    Old Miriad archive file. File last modification timestamp: 19930129   Date recorded in the file name: 19941025

--- subs/pb.h
+++ subs/pb.h
@@ -1,19 +1,16 @@
-	integer POLY,GAUS,COS6,SINGLE,IPOLY,BLOCKED
-	parameter(POLY=1,GAUS=2,COS6=3,IPOLY=4,BLOCKED=5,SINGLE=6)
-	integer MAXPB,MAXVAL,MAXOBJ
-	parameter(MAXPB=16,MAXVAL=64,MAXOBJ=2048)
 c
-	character pb(MAXPB)*16
-	integer pbtype(MAXPB),npbvals,npb,indx(MAXPB),nvals(MAXPB)
-	real f1(MAXPB),f2(MAXPB),cutoff(MAXPB),pbfwhm(MAXPB)
-	real pbvals(MAXVAL),maxrad(MAXPB)
+c Include file for pb.for
 c
-	integer pbhead
-	real xc(MAXOBJ),yc(MAXOBJ),x0(MAXOBJ),y0(MAXOBJ),fwhm(MAXOBJ)
-	real freq(MAXOBJ)
-	integer pnt(MAXOBJ)
+      include 'mirconst.h'
+      integer maxco
+      double precision rtmsq, atr
+      parameter (maxco = 5, atr = dpi / (180.0 * 3600.0),
+     +           rtmsq = 180.0 * 180.0 * 3600.0 / dpi / dpi)
 c
-	common/pb1com/ f1,f2,cutoff,pbfwhm,pbvals,xc,yc,x0,y0,fwhm,freq,
-     *	    maxrad,
-     *	  pbtype,npbvals,npb,indx,nvals,pbHead,pnt
-	common/pb1comc/pb
+      double precision pbcoeff(maxco), pbfreq
+      real pbcut, pbfac
+      character pbtype*10, pbdone*4
+c
+      common /pbdat1/ pbcoeff, pbfreq
+      common /pbdat2/ pbfac, pbcut
+      common /pbdat3/ pbtype, pbdone
