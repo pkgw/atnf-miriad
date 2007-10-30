a0dc7e4614d0af69a73d196f3e2c3bbf87e5810c
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:25:04 +0000

    Old Miriad archive file. File last modification timestamp: 19970207   Date recorded in the file name: 19970319

--- inc/miriad.h
+++ inc/miriad.h
@@ -14,8 +14,34 @@
 //   fall-96 pjt minor tidying up for AIPS++ bimafiller release	- pjt
 // 13-dec-96 rjs Regenerated, using
 //		 cproto -I$MIRINC -p -fARGS hio.c headio.c uvio.c xyio.c xyzio.c bug.c
-//
+// 07-feb-97 rjs Added "Const" to definitions, and eliminate some rubbish, as
+//		 suggested by Scott Gordon.
 */
+
+/* Define const and void if needed. */
+
+#ifdef __STDC__
+#if (__STDC__ == 1)
+typedef void Void;
+#define Const const
+#else
+typedef char Void;
+#define Const /* NULL */
+#endif /* (__STDC__ == 1) */
+#else
+typedef char Void;
+#define Const /* NULL */
+#endif /* __STDC__ */
+
+/* Define the argument list if needed. */
+
+#if !defined(ARGS)
+#  if defined(__STDC__) || defined(__cplusplus)
+#    define ARGS(s) s
+#  else
+#    define ARGS(s) ()
+#  endif
+#endif
 
 #if defined(__cplusplus)
 extern  "C" {
@@ -33,37 +59,27 @@ extern  "C" {
 #define H_TXT           6
 #define H_CMPLX         7
 
-
-#if !defined(ARGS)
-#  if defined(__STDC__) || defined(__cplusplus)
-#    define ARGS(s) s
-#  else
-#    define ARGS(s) ()
-#  endif
-#endif
-
-
 /* hio.c */
 
-void hopen_c ARGS((int *tno, char *name, char *status, int *iostat));
+void hopen_c ARGS((int *tno, Const char *name, Const char *status, int *iostat));
 void hflush_c ARGS((int tno, int *iostat));
-void habort_c ARGS((void));
+void habort_c ARGS((Void));
 void hrm_c ARGS((int tno));
 void hclose_c ARGS((int tno));
-void hdelete_c ARGS((int tno, char *keyword, int *iostat));
-void haccess_c ARGS((int tno, int *ihandle, char *keyword, char *status, int *iostat));
+void hdelete_c ARGS((int tno, Const char *keyword, int *iostat));
+void haccess_c ARGS((int tno, int *ihandle, Const char *keyword, Const char *status, int *iostat));
 void hmode_c ARGS((int tno, char *mode));
-int  hexists_c ARGS((int tno, char *keyword));
+int  hexists_c ARGS((int tno, Const char *keyword));
 void hdaccess_c ARGS((int ihandle, int *iostat));
 int  hsize_c ARGS((int ihandle));
 void hio_c ARGS((int ihandle, int dowrite, int type, char *buf, int offset, int length, int *iostat));
 void hseek_c ARGS((int ihandle, int offset));
 int  htell_c ARGS((int ihandle));
 void hreada_c ARGS((int ihandle, char *line, int length, int *iostat));
-void hwritea_c ARGS((int ihandle, char *line, int length, int *iostat));
+void hwritea_c ARGS((int ihandle, Const char *line, int length, int *iostat));
 
 /* Macros defined in hio.c */
-                                                                   */
+
 #define hreadb_c(item,buf,offset,length,iostat) \
         hio_c(item,FALSE,H_BYTE,buf,offset,length,iostat)
 #define hwriteb_c(item,buf,offset,length,iostat) \
@@ -95,27 +111,27 @@ void hwritea_c ARGS((int ihandle, char *line, int length, int *iostat));
  
 /* headio.c */
 
-void hisopen_c ARGS((int tno, char *status));
-void hiswrite_c ARGS((int tno, char *text));
+void hisopen_c ARGS((int tno, Const char *status));
+void hiswrite_c ARGS((int tno, Const char *text));
 void hisread_c ARGS((int tno, char *text, int length, int *eof));
 void hisclose_c ARGS((int tno));
-void wrhdr_c ARGS((int thandle, char *keyword, double value));
-void wrhdd_c ARGS((int thandle, char *keyword, double value));
-void wrhdi_c ARGS((int thandle, char *keyword, int value));
-void wrhdc_c ARGS((int thandle, char *keyword, float *value));
-void wrhda_c ARGS((int thandle, char *keyword, char *value));
-void rdhdr_c ARGS((int thandle, char *keyword, float *value, double defval));
-void rdhdi_c ARGS((int thandle, char *keyword, int *value, int defval));
-void rdhdd_c ARGS((int thandle, char *keyword, double *value, double defval));
-void rdhdc_c ARGS((int thandle, char *keyword, float *value, float *defval));
-void rdhda_c ARGS((int thandle, char *keyword, char *value, char *defval, int len));
-void hdcopy_c ARGS((int tin, int tout, char *keyword));
-int  hdprsnt_c ARGS((int tno, char *keyword));
-void hdprobe_c ARGS((int tno, char *keyword, char *descr, int length, char *type, int *n));
+void wrhdr_c ARGS((int tno, Const char *keyword, double value));
+void wrhdd_c ARGS((int tno, Const char *keyword, double value));
+void wrhdi_c ARGS((int tno, Const char *keyword, int value));
+void wrhdc_c ARGS((int tno, Const char *keyword, Const float *value));
+void wrhda_c ARGS((int tno, Const char *keyword, Const char *value));
+void rdhdr_c ARGS((int tno, Const char *keyword, float *value, double defval));
+void rdhdi_c ARGS((int tno, Const char *keyword, int *value, int defval));
+void rdhdd_c ARGS((int tno, Const char *keyword, double *value, double defval));
+void rdhdc_c ARGS((int tno, Const char *keyword, float *value, Const float *defval));
+void rdhda_c ARGS((int tno, Const char *keyword, char *value, Const char *defval, int len));
+void hdcopy_c ARGS((int tin, int tout, Const char *keyword));
+int  hdprsnt_c ARGS((int tno, Const char *keyword));
+void hdprobe_c ARGS((int tno, Const char *keyword, char *descr, int length, char *type, int *n));
 
 /* uvio.c */
 
-void uvopen_c ARGS((int *tno, char *name, char *status));
+void uvopen_c ARGS((int *tno, Const char *name, Const char *status));
 void uvclose_c ARGS((int tno));
 void uvflush_c ARGS((int tno));
 void uvnext_c ARGS((int tno));
@@ -123,25 +139,25 @@ void uvrewind_c ARGS((int tno));
 void uvcopyvr_c ARGS((int tin, int tout));
 int  uvupdate_c ARGS((int tno));
 void uvvarini_c ARGS((int tno, int *vhan));
-void uvvarset_c ARGS((int vhan, char *var));
+void uvvarset_c ARGS((int vhan, Const char *var));
 void uvvarcpy_c ARGS((int vhan, int tout));
 int  uvvarupd_c ARGS((int vhan));
-void uvrdvr_c ARGS((int tno, int type, char *var, char *data, char *def, int n));
-void uvgetvr_c ARGS((int tno, int type, char *var, char *data, int n));
-void uvprobvr_c ARGS((int tno, char *var, char *type, int *length, int *updated));
-void uvputvr_c ARGS((int tno, int type, char *var, char *data, int n));
-void uvtrack_c ARGS((int tno, char *name, char *switches));
-int  uvscan_c ARGS((int tno, char *var));
-void uvwrite_c ARGS((int tno, double *preamble, float *data, int *flags, int n));
-void uvwwrite_c ARGS((int tno, float *data, int *flags, int n));
-void uvsela_c ARGS((int tno, char *object, char *string, int datasel));
-void uvselect_c ARGS((int tno, char *object, double p1, double p2, int datasel));
-void uvset_c ARGS((int tno, char *object, char *type, int n, double p1, double p2, double p3));
-void uvread_c ARGS((int tno, double preamble[4], float *data, int *flags, int n, int *nread));
+void uvrdvr_c ARGS((int tno, int type, Const char *var, char *data, char *def, int n));
+void uvgetvr_c ARGS((int tno, int type, Const char *var, char *data, int n));
+void uvprobvr_c ARGS((int tno, Const char *var, char *type, int *length, int *updated));
+void uvputvr_c ARGS((int tno, int type, Const char *var, Const char *data, int n));
+void uvtrack_c ARGS((int tno, Const char *name, Const char *switches));
+int  uvscan_c ARGS((int tno, Const char *var));
+void uvwrite_c ARGS((int tno, Const double *preamble, Const float *data, Const int *flags, int n));
+void uvwwrite_c ARGS((int tno, Const float *data, Const int *flags, int n));
+void uvsela_c ARGS((int tno, Const char *object, Const char *string, int datasel));
+void uvselect_c ARGS((int tno, Const char *object, double p1, double p2, int datasel));
+void uvset_c ARGS((int tno, Const char *object, Const char *type, int n, double p1, double p2, double p3));
+void uvread_c ARGS((int tno, double *preamble, float *data, int *flags, int n, int *nread));
 void uvwread_c ARGS((int tno, float *data, int *flags, int n, int *nread));
-void uvflgwr_c ARGS((int tno, int *flags));
-void uvwflgwr_c ARGS((int tno, int *flags));
-void uvinfo_c ARGS((int tno, char *object, double *data));
+void uvflgwr_c ARGS((int tno, Const int *flags));
+void uvwflgwr_c ARGS((int tno, Const int *flags));
+void uvinfo_c ARGS((int tno, Const char *object, double *data));
 
 /* Macros defined in uvio.c */
 
@@ -160,46 +176,46 @@ void uvinfo_c ARGS((int tno, char *object, double *data));
 
 /* xyio.c */
 
-void xyopen_c ARGS((int *thandle, char *name, char *status, int naxis, int axes[]));
-void xyflush_c ARGS((int thandle));
-void xyclose_c ARGS((int thandle));
-void xyread_c ARGS((int thandle, int index, float array[]));
-void xywrite_c ARGS((int thandle, int index, float array[]));
-void xymkrd_c ARGS((int thandle, int index, int runs[], int n, int *nread));
-void xymkwr_c ARGS((int thandle, int index, int runs[], int n));
-void xyflgwr_c ARGS((int thandle, int index, int flags[]));
-void xyflgrd_c ARGS((int thandle, int index, int flags[]));
-void xysetpl_c ARGS((int thandle, int naxis, int axes[]));
+void xyopen_c ARGS((int *tno, Const char *name, Const char *status, int naxis, int axes[]));
+void xyflush_c ARGS((int tno));
+void xyclose_c ARGS((int tno));
+void xyread_c ARGS((int tno, int index, float *array));
+void xywrite_c ARGS((int tno, int index, Const float *array));
+void xymkrd_c ARGS((int tno, int index, int *runs, int n, int *nread));
+void xymkwr_c ARGS((int tno, int index, Const int *runs, int n));
+void xyflgwr_c ARGS((int tno, int index, Const int *flags));
+void xyflgrd_c ARGS((int tno, int index, int *flags));
+void xysetpl_c ARGS((int tno, int naxis, Const int *axes));
 
 /* xyzio.c */
 
-void xyzopen_c ARGS((int *handle, char *name, char *status, int *naxis, int axlen[]));
+void xyzopen_c ARGS((int *tno, Const char *name, Const char *status, int *naxis, int axlen[]));
 void xyzclose_c ARGS((int tno));
 void xyzflush_c ARGS((int tno));
-void xyzsetup_c ARGS((int tno, char *subcube, int blc[], int trc[], int viraxlen[], int vircubesize[]));
+void xyzsetup_c ARGS((int tno, Const char *subcube, Const int blc[], Const int trc[], int viraxlen[], int vircubesize[]));
 void xyzs2c_c ARGS((int tno, int subcubenr, int coords[]));
-void xyzc2s_c ARGS((int tno, int coords[], int *subcubenr));
-void xyzread_c ARGS((int tno, int coords[], float *data, int *mask, int *ndata));
+void xyzc2s_c ARGS((int tno, Const int coords[], int *subcubenr));
+void xyzread_c ARGS((int tno, Const int coords[], float *data, int *mask, int *ndata));
 void xyzpixrd_c ARGS((int tno, int pixelnr, float *data, int *mask));
 void xyzprfrd_c ARGS((int tno, int profilenr, float *data, int *mask, int *ndata));
 void xyzplnrd_c ARGS((int tno, int planenr, float *data, int *mask, int *ndata));
-void xyzwrite_c ARGS((int tno, int coords[], float *data, int *mask, int *ndata));
-void xyzpixwr_c ARGS((int tno, int pixelnr, float *data, int *mask));
-void xyzprfwr_c ARGS((int tno, int profilenr, float *data, int *mask, int *ndata));
-void xyzplnwr_c ARGS((int tno, int planenr, float *data, int *mask, int *ndata));
+void xyzwrite_c ARGS((int tno, Const int coords[], Const float *data, Const int *mask, Const int *ndata));
+void xyzpixwr_c ARGS((int tno, int pixelnr, Const float *data, Const int *mask));
+void xyzprfwr_c ARGS((int tno, int profilenr, Const float *data, Const int *mask, Const int *ndata));
+void xyzplnwr_c ARGS((int tno, int planenr, Const float *data, Const int *mask, Const int *ndata));
 
 /* bug.c */
 
-void buglabel_c ARGS((char *name));
+void buglabel_c ARGS((Const char *name));
 void bugno_c ARGS((char s, int n));
-void bug_c ARGS((char s, char *m));
+void bug_c ARGS((char s, Const char *m));
 
 /* scrio.c */
 
 void scropen_c ARGS((int *handle));
 void scrclose_c ARGS((int handle));
 void scrread_c ARGS((int handle, float *buffer, int offset, int length));
-void scrwrite_c ARGS((int handle, float *buffer, int offset, int length));
+void scrwrite_c ARGS((int handle, Const float *buffer, int offset, int length));
 
 #if defined(__cplusplus)
 }
