a63670232f8c4598940a8042cfa7de10c38388a7
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:27:42 +0000

    Old Miriad archive file. File last modification timestamp: 19951204   Date recorded in the file name: 20050128

--- subs/pb.h
+++ subs/pb.h
@@ -1,9 +1,9 @@
-	integer POLY,GAUS,COS6,SINGLE
-	parameter(POLY=1,GAUS=2,COS6=3,SINGLE=4)
+	integer POLY,GAUS,COS6,SINGLE,IPOLY,BLOCKED
+	parameter(POLY=1,GAUS=2,COS6=3,IPOLY=4,BLOCKED=5,SINGLE=6)
 	integer MAXPB,MAXVAL,MAXOBJ
 	parameter(MAXPB=16,MAXVAL=64,MAXOBJ=2048)
 c
-	character pb(MAXPB)*16
+	character pb(MAXPB)*16,descrip(MAXPB)*32
 	integer pbtype(MAXPB),npbvals,npb,indx(MAXPB),nvals(MAXPB)
 	real f1(MAXPB),f2(MAXPB),cutoff(MAXPB),pbfwhm(MAXPB)
 	real pbvals(MAXVAL),maxrad(MAXPB)
@@ -16,4 +16,4 @@ c
 	common/pb1com/ f1,f2,cutoff,pbfwhm,pbvals,xc,yc,x0,y0,fwhm,freq,
      *	    maxrad,
      *	  pbtype,npbvals,npb,indx,nvals,pbHead,pnt
-	common/pb1comc/pb
+	common/pb1comc/pb,descrip
