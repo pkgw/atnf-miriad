0c3cfb5e45664b328c549b307e4a99095134f539
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:27:15 +0000

    Old Miriad archive file. File last modification timestamp: 19950207   Date recorded in the file name: 19970326

--- subs/mostab.h
+++ subs/mostab.h
@@ -4,14 +4,14 @@
 	integer pntno,npnt,vPntUpd,HashSize,Hash(MAXHASH),nx2,ny2
 	integer pbObj(MAXPNT)
 	logical solar,doinit
-	real ucoeff(3,MAXPNT),vcoeff(3,MAXPNT)
+	double precision ucoeff(3,MAXPNT),vcoeff(3,MAXPNT)
 	real Rms2(MAXPNT),SumWt(MAXPNT),x0(MAXPNT),y0(MAXPNT)
 	double precision llmm(2,MAXPNT),radec(2,MAXPNT),radec0(2)
 	double precision cdelt1,cdelt2
 	character telescop(MAXPNT)*16
 	real pbfwhm(MAXPNT)
 	common/mostab1/	llmm,radec,radec0,cdelt1,cdelt2,
-     *	  pbfwhm,ucoeff,vcoeff,Rms2,SumWt,x0,y0,
+     *	  ucoeff,vcoeff,pbfwhm,Rms2,SumWt,x0,y0,
      *	  pntno,npnt,nxy,pX,pY,vPntUpd,HashSize,Hash,nx2,ny2,coRef,
      *	  pbObj,
      *	  solar,doinit
