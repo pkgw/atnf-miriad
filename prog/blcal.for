120d5d54ea596e2cdc89da151cbfeefc276aeab8
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:13:51 +0000

    Old Miriad archive file. File last modification timestamp: 19970316   Date recorded in the file name: 19970317

---
+++ prog/blcal.for
@@ -0,0 +1,465 @@
+c************************************************************************
+	program blcal
+	implicit none
+c
+c= blcal - Compute and apply baseline calibration.
+c& rjs
+c: uv analysis
+c+
+c	UVAVER copies a uv dataset, performing averaging, both in time
+c	and/or frequency.
+c@ vis
+c	The name of the input uv data sets. No default.
+c@ select
+c	The normal uv selection commands. The default is copy everything.
+c@ ref
+c	Reference visibility dataset. This dataset is used to determine
+c	the baseline gain function.
+c@ line
+c	The normal uv linetype in the form:
+c	  line,nchan,start,width,step
+c	The default is all channels (or all wide channels if there are no
+c	spectral channels). The output will consist of only spectral or
+c	wideband data (but not both).
+c@ interval
+c	Solution time interval, in minutes. The default is 5 minutes.
+c@ out
+c	The name of the output uv data set. No default.
+c--
+c  History:
+c    rjs  14mar97 Original version.
+c
+c------------------------------------------------------------------------
+	include 'maxdim.h'
+	character version*(*)
+	integer MAXSELS
+	parameter(version='BlCal: version 1.0 14-Mar-97')
+	parameter(MAXSELS=200)
+	character vis*64,ref*64,out*64,line*32
+	integer lVis,lRef,lOut
+	integer nchan,pol,npol
+	double precision interval,preamble(6)
+	real lstart,lwidth,lstep,sels(MAXSELS)
+	complex data(MAXCHAN)
+	logical flags(MAXCHAN)
+c
+c  Get the inputs.
+c
+	call output(version)
+	call keyini
+	call keya('vis',vis,' ')
+	if(vis.eq.' ')call bug('f','Input vis file must be given')
+	call keya('ref',ref,' ')
+	if(ref.eq.' ')call bug('f','Input ref file must be given')
+	call keyline(line,nchan,lstart,lwidth,lstep)
+	if(line.eq.' ')line = 'channel'
+	call selinput('select',sels,MAXSELS)
+	call keyd('interval',interval,5.d0)
+	if(interval.le.0)call bug('f','Invalid value for interval')
+	interval = interval/(60.*24.)
+	call keya('out',out,' ')
+	if(out.eq.' ')call bug('f','An output must be given')
+	call keyfin
+c
+c  Open the reference, get the solution, and close.
+c
+	call output('Calculating the baseline gains ...')
+	call uvopen(lRef,ref,'old')
+	call uvset(lRef,'data',line,nchan,lstart,lwidth,lstep)
+	call uvset(lRef,'preamble','time/baseline/pol',0,0.,0.,0.)
+	call datLoad(lRef,interval)
+	call uvclose(lRef)
+	call datNorm
+c
+c  Open the input and output.
+c
+	call output('Applying the baseline gains ...')
+	call uvopen(lVis,vis,'old')
+	call selApply(lVis,sels,.true.)
+	call uvset(lVis,'preamble','uvw/time/baseline',0,0.,0.,0.)
+	call varInit(lVis,line)
+	call uvopen(lOut,out,'new')
+	call uvset(lOut,'preamble','uvw/time/baseline',0,0.,0.,0.)
+	call varOnit(lVis,lOut,line)
+c
+	call uvread(lVis,preamble,data,flags,MAXCHAN,nchan)
+	dowhile(nchan.gt.0)
+	  call uvrdvri(lVis,'pol',pol,0)
+	  call uvrdvri(lVis,'npol',npol,1)
+	  preamble(6) = pol
+	  call DatCorr(preamble(4),data,flags,nchan)
+	  call varCopy(lVis,lOut)
+	  if(pol.ne.0)then
+	    call uvputvri(lOut,'pol',pol,1)
+	    call uvputvri(lOut,'npol',npol,1)
+	  endif
+	  call uvwrite(lOut,preamble,data,flags,nchan)
+	  call uvread(lVis,preamble,data,flags,MAXCHAN,nchan)
+	enddo
+c
+	call hdcopy(lVis,lOut,'history')
+	call hisOpen(lOut,'append')
+	call hisWrite(lOut,'BLCAL: Miriad '//version)
+	call hisInput(lOut,'BLCAL')
+	call hisClose(lOut)
+c
+	call uvclose(lVis)
+	call uvclose(lOut)
+	end
+c************************************************************************
+	subroutine datCorr(co,data,flags,nchan1)
+c
+	implicit none
+	integer nchan1
+	double precision co(3)
+	complex data(nchan1)
+	logical flags(nchan1)
+c------------------------------------------------------------------------
+	include 'blcal.h'
+	integer pol,bl,i1,i2,i
+	logical more
+c
+	if(nchan.ne.nchan1)call bug('f','Channel mismatch')
+c
+	pol = nint(co(3)) + 9
+	if(pol.le.0.or.pol.gt.MAXPOL)
+     *	  call bug('f','Invalid polarisation')
+	call basant(co(2),i1,i2)
+	bl = ((i2-1)*i2)/2 + i1
+c
+c  Search for the appropriate solution.
+c
+	i1 = head(pol,bl)
+	i2 = i1
+	more = .true.
+	dowhile(i2.ne.0.and.more)
+	  if(time(i1).gt.co(1).or.co(1).gt.time(i2))then
+	    i1 = i2
+	    i2 = next(i2)
+	  else
+	    more = .false.
+	  endif
+	enddo
+c
+c  If one was found, then apply it.
+c
+	if(.not.more)then
+	  call datApply(memc(gidx(i1)),memi(fidx(i1)),time(i1),
+     *			memc(gidx(i2)),memi(fidx(i2)),time(i2),
+     *			data,flags,co(1),nchan)
+	else
+	  do i=1,nchan
+	    flags(i) = .false.
+	  enddo
+	endif
+c
+	end
+c************************************************************************
+	subroutine datApply(vis1,cnt1,t1,vis2,cnt2,t2,
+     *			data,flags,time,nchan)
+c
+	implicit none
+	integer nchan
+	double precision t1,t2,time
+	complex vis1(nchan),vis2(nchan),data(nchan)
+	integer cnt1(nchan),cnt2(nchan)
+	logical flags(nchan)
+c
+c------------------------------------------------------------------------
+	integer i
+	real a,b
+	complex temp
+c
+	if(time.eq.t1)then
+	  do i=1,nchan
+	    if(cnt1(i).gt.0)then
+	      data(i) = data(i)/vis1(i)
+	    else
+	      flags(i) = .false.
+	    endif
+	  enddo
+	else if(time.eq.t2)then
+	  do i=1,nchan
+	    if(cnt2(i).gt.0)then
+	      data(i) = data(i)/vis2(i)
+	    else
+	      flags(i) = .false.
+	    endif
+	  enddo
+	else
+	  a = 1 - (time-t1)/(t2-t1)
+	  b = 1 - a
+	  do i=1,nchan
+	    if(cnt1(i)*cnt2(i).gt.0)then
+	      temp = a*vis1(i) + b*vis2(i)
+	      data(i) = data(i)/temp
+	    else
+	      flags(i) = .false.
+	    endif
+	  enddo
+	endif
+c
+	end
+c************************************************************************
+	subroutine datLoad(lVis,interval)
+c
+	implicit none
+	integer lVis
+	double precision interval
+c------------------------------------------------------------------------
+	include 'blcal.h'
+	double precision co(3)
+	integer pol,bl,pmin,pmax,bmin,bmax,nread,ngood,i,j,i1,i2
+	integer Tail(MAXPOL,MAXBASE)
+	logical buffered
+	double precision t,tmin,tmax
+	complex data(MAXCHAN)
+	logical flags(MAXCHAN)
+c
+c  Initialise
+c
+	nsols = 0
+	do j=1,MAXBASE
+	  do i=1,MAXPOL
+	    Head(i,j) = 0
+	    Tail(i,j) = 0
+	  enddo
+	enddo
+c
+	call uvread(lVis,co,data,flags,MAXCHAN,nread)
+	nchan = nread
+	buffered = .false.
+	tmin = 0
+	tmax = 0
+	dowhile(nchan.eq.nread)
+	  ngood = 0
+	  do i=1,nchan
+	    if(flags(i))ngood = ngood + 1
+	  enddo
+	  if(ngood.gt.0)then
+c
+c  Get the parameters of this record.
+c
+	    pol = nint(co(3)) + 9
+	    if(pol.le.0.or.pol.gt.MAXPOL)
+     *	      call bug('f','Invalid polarisation')
+	    call basant(co(2),i1,i2)
+	    bl = ((i2-1)*i2)/2 + i1
+	    t = co(1)
+c
+c  Is this the end of an integration.
+c
+	    if(buffered.and.(t-tmin.gt.interval.or.
+     *			     tmax-t.gt.interval))then
+	      call datFlush(Tail,MAXPOL,MAXBASE,pmin,pmax,bmin,bmax)
+	      buffered = .false.
+	    endif
+c
+	    if(tail(pol,bl).eq.0)then
+	      nsols = nsols + 1
+	      tail(pol,bl) = nsols
+	      call memAlloc(gidx(nsols),nchan,'c')
+	      call ZeroC(memc(gidx(nsols)),nchan)
+	      call memAlloc(fidx(nsols),nchan,'i')
+	      call ZeroI(memi(fidx(nsols)),nchan)
+	      time(nsols) = 0
+	    endif
+	    i = tail(pol,bl)
+c
+c  Add in this solution.
+c
+ 	    call datAdd(memc(gidx(i)),memi(fidx(i)),time(i),
+     *		data,flags,co,nchan)
+c
+c  Book keeping.
+c	
+	    if(.not.buffered)then
+	      pmin = pol
+	      pmax = pmin
+	      bmin = bl
+	      bmax = bmin
+	      tmin = t
+	      tmax = tmin
+	    else
+	      pmin = min(pmin,pol)
+	      pmax = max(pmax,pol)
+	      bmin = min(bmin,bl)
+	      bmax = max(bmax,bl)
+	      tmin = min(tmin,t)
+	      tmax = max(tmax,t)
+	    endif
+	    buffered = .true.
+	  endif
+c
+c  Go back for more.
+c
+	  call uvread(lVis,co,data,flags,MAXCHAN,nread)
+	enddo
+c
+	if(buffered)call datFlush(Tail,MAXPOL,MAXBASE,pmin,pmax,
+     *							bmin,bmax)
+c
+	end
+c************************************************************************
+	subroutine datFlush(Tail,MpOL,MBASE,pmin,pmax,bmin,bmax)
+c
+	implicit none
+	integer MPOL,MBASE,pmin,pmax,bmin,bmax
+	integer Tail(MPOL,MBASE)
+c------------------------------------------------------------------------
+	include 'blcal.h'
+	integer b,p,i
+c
+	do b=bmin,bmax
+	  do p=pmin,pmax
+	    i = Tail(p,b)
+	    if(i.ne.0)call datLink(Head(p,b),i)
+	    Tail(p,b) = 0
+	  enddo
+	enddo
+c
+	end
+c************************************************************************
+	subroutine datNorm
+c
+	implicit none
+c------------------------------------------------------------------------
+	include 'blcal.h'
+	double precision Sum
+	integer cnt,p,b,pnt
+	real g
+c
+	Sum = 0
+	Cnt = 0
+	do b=1,MAXBASE
+	  do p=1,MAXPOL
+	    pnt = Head(p,b)
+	    dowhile(pnt.ne.0)
+	      call datNorm1(memc(gidx(pnt)),memi(fidx(pnt)),
+     *					time(pnt),nchan,Sum,Cnt)
+	      pnt = Next(pnt)
+	    enddo
+	  enddo
+	enddo
+c
+	if(cnt.eq.0)call bug('f','No good data found')
+c
+	g = sqrt(Cnt/Sum)
+	do b=1,MAXBASE
+	  do p=1,MAXPOL
+	    pnt = Head(p,b)
+	    dowhile(pnt.ne.0)
+	      call datNorm2(memc(gidx(pnt)),nchan,g)
+	      pnt = Next(pnt)
+	    enddo
+	  enddo
+	enddo
+c
+	end
+c************************************************************************
+	subroutine datNorm1(vis,cnt,time,nchan,Sum,SumCnt)
+c
+	implicit none
+	integer nchan,cnt(nchan),SumCnt
+	complex vis(nchan)
+	double precision time,Sum
+c------------------------------------------------------------------------
+	integer i,n
+c
+	n = 0
+	do i=1,nchan
+	  n = n + cnt(i)
+	  if(cnt(i).gt.0)then
+	    sum = sum + 
+     *		(real(vis(i))**2 + aimag(vis(i))**2)/cnt(i)
+	    vis(i) = vis(i) / cnt(i)
+	  else
+	    vis(i) = 1
+	  endif
+	enddo
+	SumCnt = SumCnt + n
+	time = time / n
+c
+	end
+c************************************************************************
+	subroutine datNorm2(vis,nchan,g)
+c
+	implicit none
+	integer nchan
+	complex vis(nchan)
+	real g
+c------------------------------------------------------------------------
+	integer i
+c
+	do i=1,nchan
+	  vis(i) = g*vis(i)
+	enddo
+c
+	end
+c************************************************************************
+	subroutine datLink(Hd,pnt)
+c
+	implicit none
+	integer Hd,pnt
+c------------------------------------------------------------------------
+	include 'blcal.h'
+	integer i
+c
+	if(hd.eq.0)then
+	  hd = pnt
+	else
+	  i = hd
+	  dowhile(next(i).ne.0)
+	    i = next(i)
+	  enddo
+	  next(i) = pnt
+	endif
+c
+	end
+c************************************************************************
+	subroutine datAdd(vis,cnt,t,data,flags,time,nchan)
+c
+	implicit none
+	integer nchan
+	double precision t,time
+	logical flags(nchan)
+	complex vis(nchan),data(nchan)
+	integer cnt(nchan)
+c------------------------------------------------------------------------
+	integer i
+c
+	do i=1,nchan
+	  if(flags(i))then
+	    t = t + time
+	    vis(i) = vis(i) + data(i)
+	    cnt(i) = cnt(i) + 1
+	  endif
+	enddo
+c
+	end
+c************************************************************************
+	subroutine zeroc(data,n)
+c
+	implicit none
+	integer n
+	complex data(n)
+c------------------------------------------------------------------------
+	integer i
+c
+	do i=1,n
+	  data(i) = 0
+	enddo
+	end
+c************************************************************************
+	subroutine zeroi(data,n)
+c
+	implicit none
+	integer n
+	integer data(n)
+c------------------------------------------------------------------------
+	integer i
+c
+	do i=1,n
+	  data(i) = 0
+	enddo
+	end
