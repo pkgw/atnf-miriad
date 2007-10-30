b166e202ef9ca9351482c8761a3942a47c7d716c
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:21:10 +0000

    Old Miriad archive file. File last modification timestamp: 19950222   Date recorded in the file name: 19960805

--- prog/uvsplit.h
+++ prog/uvsplit.h
@@ -1,11 +1,12 @@
+	include 'maxdim.h'
 	integer MAXFILES,MAXOPEN
 	parameter(MAXFILES=256,MAXOPEN=6)
 c
-	integer npol,nopen,nfiles,ifno(MAXFILES),indx(MAXFILES)
+	integer npol,nopen,nfiles,ifno(MAXFILES),indx(MAXFILES),nwins
 	integer vCheck(MAXFILES),vCopy(MAXFILES),lOut(MAXFILES),lVis
-	logical done(MAXFILES),doif,dowide,docomp
+	logical done(MAXFILES),doif,dowide,docomp,wins(MAXWIN)
 	character out(MAXFILES)*64,version*64
 c
 	common/Files/npol,nopen,nfiles,ifno,indx,vCheck,vCopy,
-     *	    lOut,lVis,done,doif,dowide,docomp
+     *	    lOut,lVis,nwins,done,doif,dowide,docomp,wins
 	common/FilesC/out,version
