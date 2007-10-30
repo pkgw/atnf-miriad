46409105b81811b436b28e4a009e67f9c350a630
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:21:08 +0000

    Old Miriad archive file. File last modification timestamp: 19950922   Date recorded in the file name: 19951005

--- prog/uvsplit.for
+++ prog/uvsplit.for
@@ -58,6 +58,7 @@ c    rjs  29aug94 W-axis change.
 c    rjs   6sep94 Use MAXWIN in maxdim.h. Better treatment of xyphase.
 c    rjs  25jan95 Added options=mosaic.
 c    rjs  21feb95 Get select=win to work.
+c    rjs  22sep95 Re-added support for pulsar binning.
 c
 c  Bugs:
 c   Perfect?
@@ -683,7 +684,7 @@ c------------------------------------------------------------------------
 	character line*64
 c
 	integer NCOPY,NSCHECK,NWCHECK
-	parameter(NCOPY=63,NSCHECK=8,NWCHECK=3)
+	parameter(NCOPY=64,NSCHECK=8,NWCHECK=3)
 	character copy(NCOPY)*8,scheck(NSCHECK)*8,wcheck(NWCHECK)*8
         data copy/    'airtemp ','antdiam ','antpos  ','atten   ',
      *     'axisrms ','chi     ','corbit  ','corbw   ','corfin  ',
@@ -697,7 +698,7 @@ c
      *     'plmin   ','pltb    ','precipmm','ra      ','relhumid',
      *     'source  ','telescop','temp    ','tpower  ','ut      ',
      *     'veldop  ','veltype ','version ','vsource ','winddir ',
-     *     'windmph ','delay0  ','npol    ','pol     '/
+     *     'windmph ','delay0  ','npol    ','pol     ','bin     '/
 c
 	data SCheck/  'nspect  ','restfreq','ischan  ','nschan  ',
      *     'sfreq   ','sdf     ','systemp ','xyphase '/
