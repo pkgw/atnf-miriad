380ba66015d692ac08f7f11869958c5a0493055b
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:20:52 +0000

    Old Miriad archive file. File last modification timestamp: 20000310   Date recorded in the file name: 20000328

--- prog/uvplt.for
+++ prog/uvplt.for
@@ -308,6 +308,7 @@ c    nebk 18jun98  Document nofqav options better
 c    rjs  19nov98  Minor correction to computation of LST, and recompute from
 c		   scratch the parallactic angle.
 c    swa  03sep99  Add options=notitle.
+c    rjs  10mar00  Set maxbase2=91.
 c
 c To do:
 c
@@ -369,7 +370,7 @@ c
       include 'mirconst.h'
 c
       integer maxbase2, maxco, maxpol, maxfile
-      parameter (maxbase2 = 36, maxco = 7, maxpol = 4, maxfile = 30)
+      parameter (maxbase2 = 91, maxco = 7, maxpol = 4, maxfile = 30)
 c
       complex data(maxchan)
       double precision freq(maxchan)
@@ -438,7 +439,7 @@ c
       data npts, plpts, basmsk /ifac1*0, ifac1*0, ifac2*0/
       data polmsk /13*0/
 c-----------------------------------------------------------------------
-      call output ('UvPlt: version 30-Jul-96')
+      call output ('UvPlt: version 10-Mar-00')
 c
 c  Get the parameters given by the user and check them for blunders
 c
