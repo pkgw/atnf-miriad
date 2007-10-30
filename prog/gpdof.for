86d5de2395c2566cf3d073a5dc560bef8709fbdb
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:16:20 +0000

    Old Miriad archive file. File last modification timestamp: 20010529   Date recorded in the file name: 20010601

--- prog/gpdof.for
+++ prog/gpdof.for
@@ -825,11 +825,11 @@ c     *  dofstd(1),'+/-',dofstd(2),' ',
 c     *  dofstd(3),'+/-',dofstd(4),' ',
 c     *  dofstd(5),'+/-',dofstd(6)
 	write (string,
-     *  '(1x,a5,a4,f9.6,a2,a7,a3,f9.6,a2,a7,a3,f9.6,a3,a7)') 
+     *  '(1x,a5,a4,f8.5,a3,a7,a3,f8.5,a3,a7,a3,f8.5,a3,a7)') 
      *  'std  ', ' : ',
-     *  dofstd(1),' ',' ',' ',
-     *  dofstd(3),' ',' ',' ',
-     *  dofstd(5),' ',' '
+     *  dofstd(1),'','',' ',
+     *  dofstd(3),'','',' ',
+     *  dofstd(5),'',''
 	call output(string)
 	
 c
@@ -844,11 +844,11 @@ c
 	end do
 
 	write (string,
-     *  '(1x,a9,f9.6,a2,a7,a3,f9.6,a2,a7,a3,f9.6,a2,a7)') 
+     *  '(1x,a9,f8.5,a3,a7,a3,f8.5,a3,a7,a3,f8.5,a3,a7)') 
      *  'std err: ',
-     *  dofstd(1),' ',' ',' ',
-     *  dofstd(2),' ',' ',' ',
-     *  dofstd(3),' ',' '
+     *  dofstd(1),'','',' ',
+     *  dofstd(2),'','',' ',
+     *  dofstd(3),'',''
 	call output(string)
 	
 
