741ce72eff36b1ced741d107ea8b89a7235b959e
Bob Sault <rsault@nrao.edu>
2009-04-08 15:25:16 +0000

    Changes to support scrRecSz routine

--- subs/scrio.c
+++ subs/scrio.c
@@ -5,16 +5,27 @@
 /*  History:								*/
 /*   rjs Dark-ages Original version.					*/
 /*   rjs   6nov94  Change item handle to an integer.			*/
+/*   rjs  26oct95  Better messages on errors.				*/
+/*   pjt  19jun02  MIR4 prototypes                                      */
+/*   jwr  05nov04  Change file offsets to type off_t			*/
+/*   rjs  03jan05  Include file rationalisation.                        */
+/*   pjt  16feb07  Minor doc improvements                               */
+/*   pjt  11dec07  More helpful message when scratch files fail         */
+/*   rjs  01apr09  Add scrRecSz routine and associated work.		*/
 /************************************************************************/
 
+#include <stdio.h>
+#include "miriad.h"
 #include "io.h"
 
-char *sprintf();
 
+#define MAXITEMS 100
 static int number=0;
+static int items[MAXITEMS],first;
+size_t recsiz[MAXITEMS];
+
 /************************************************************************/
-void scropen_c(handle)
-int *handle;
+void scropen_c(int *handle)
 /**scropen -- Open a scratch file.					*/
 /*:scratch-i/o								*/
 /*+  FORTRAN call sequence:
@@ -23,21 +34,47 @@ int *handle;
 	integer tno
 
   This opens a scratch file, and readies it for use.
+  Scratch files will be removed when they are closed, multiple scratch
+  files are allowed, and they always live in the current directory, unless
+  the $TMPDIR environment variable points to another directory.
+
   Output:
     tno		The handle of the scratch file.				*/
 /*--									*/
 /*----------------------------------------------------------------------*/
 {
-  int iostat;
+  int iostat,temp,i;
   char name[32];
 
+/* Initialise the first time through. */
+
+  if(number == 0){
+    for(i=0;i<MAXITEMS-1;i++){
+      items[i] = i+1;
+    }
+    items[MAXITEMS-1] = -1;
+    first = 0;
+  }
+
+/* Get a handle. */
+
+  if(first < 0)bug_c('w',"Exhausted the number of open scratch files");
+  *handle = first;
+  first = items[first];
+
+/* Open the scratch file. */
+
   (void)sprintf(name,"scratch%d",number++);
-  haccess_c(0,handle,name,"scratch",&iostat);
-  if(iostat) bugno_c('f',iostat);
+  haccess_c(0,&temp,name,"scratch",&iostat);
+  if(iostat){
+    bug_c(  'w',"Error opening scratch file; check your $TMPDIR");
+    bugno_c('f',iostat);
+  }
+  items[*handle] = temp;
+  recsiz[*handle] = sizeof(float);
 }
 /************************************************************************/
-void scrclose_c(handle)
-int handle;
+void scrclose_c(int handle)
 /**scrclose -- Close and delete a scratch file.				*/
 /*:scratch-i/o								*/
 /*+  FORTRAN call sequence:
@@ -54,13 +91,35 @@ int handle;
 {
   int iostat;
 
-  hdaccess_c(handle,&iostat);
-  if(iostat) bugno_c('f',iostat);
+  hdaccess_c(items[handle],&iostat);
+  items[handle] = first;
+  first = handle;
+  if(iostat){
+    bug_c(  'w',"Error closing scratch file");
+    bugno_c('f',iostat);
+  }
 }
 /************************************************************************/
-void scrread_c(handle,buffer,offset,length)
-int handle,offset,length;
-float *buffer;
+void scrrecsz_c(int handle,size_t recsize)
+/**scrrecsz -- Set record size to be used.				*/
+/*:scratch-i/o								*/
+/*+  FORTRAN call sequence:
+
+	subroutine scrrecsz(tno,recsize)
+	integer tno,recsize
+
+  This sets the record size to be used in future access operations.
+  Input:
+    tno		The handle of the scratch file.
+    recsize	The record size.					*/
+/*--									*/
+/*----------------------------------------------------------------------*/
+{
+  if(recsize <= 0)bug_c('f',"Invalid record size, in scrrecsz");
+  recsiz[handle] = recsize*sizeof(float);
+}
+/************************************************************************/
+void scrread_c(int handle,float *buffer,off_t offset,size_t length)
 /**scrread -- Read real data from a scratch file.			*/
 /*:scratch-i/o								*/
 /*+  FORTRAN call sequence:
@@ -81,15 +140,22 @@ float *buffer;
 /*----------------------------------------------------------------------*/
 {
   int iostat;
+  int myhandle;
+  off_t myoff;
+  size_t mylen;
 
-  hreadb_c(handle,(char *)buffer,
-    sizeof(float)*offset,sizeof(float)*length,&iostat);
-  if(iostat) bugno_c('f',iostat);
+  myhandle = items[handle];
+  mylen    = recsiz[handle]*length;
+  myoff    = recsiz[handle]*offset;
+
+  hreadb_c(myhandle,(char *)buffer,myoff,mylen,&iostat);
+  if(iostat){
+    bug_c(  'w',"Error reading from scratch file");
+    bugno_c('f',iostat);
+  }
 }
 /************************************************************************/
-void scrwrite_c(handle,buffer,offset,length)
-int handle,offset,length;
-float *buffer;
+void scrwrite_c(int handle,Const float *buffer,off_t offset,size_t length)
 /**scrwrite -- Write real data to the scratch file.			*/
 /*:scratch-i/o								*/
 /*+  FORTRAN call sequence:
@@ -109,8 +175,17 @@ float *buffer;
 /*----------------------------------------------------------------------*/
 {
   int iostat;
+  int myhandle;
+  off_t myoff;
+  size_t mylen;
 
-  hwriteb_c(handle,(char *)buffer,
-    sizeof(float)*offset,sizeof(float)*length,&iostat);
-  if(iostat) bugno_c('f',iostat);
+  myhandle = items[handle];
+  mylen    = recsiz[handle]*length;
+  myoff    = recsiz[handle]*offset;
+
+  hwriteb_c(myhandle,(char *)buffer,myoff,mylen,&iostat);
+  if(iostat){
+    bug_c(  'w',"Error writing to scratch file; check your $TMPDIR");
+    bugno_c('f',iostat);
+  }
 }
