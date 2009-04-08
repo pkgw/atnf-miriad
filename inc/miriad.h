5420e8db9ce7ab8e78df8b274673baa9cfac36c4
Bob Sault <rsault@nrao.edu>
2009-04-08 15:21:54 +0000

    Add interface definition for scrRecSz

--- inc/miriad.h
+++ inc/miriad.h
@@ -24,6 +24,7 @@
 // 23-jun-02 pjt define MIR4 here if you want to enable the LSF and MIR4
 // 30-aug-04 pjt removed deprecated ARGS() macro
 // 01-jan-04 rjs Eliminate MIR3/MIR4 stuff. Tidy up.
+// 01-apr-09 rjs Add additional interface to scrRecSz
 */
 
 
@@ -175,6 +176,7 @@ void uvclose_c  (int tno);
 void uvflush_c  (int tno);
 void uvnext_c   (int tno);
 void uvrewind_c (int tno);
+int  uvdim_c    (int tno);
 void uvcopyvr_c (int tin, int tout);
 int  uvupdate_c (int tno);
 void uvvarini_c (int tno, int *vhan);
@@ -242,6 +244,7 @@ void uvinfo_c   (int tno, Const char *object, double *data);
 void xyopen_c  (int *tno, Const char *name, Const char *status, int naxis, int axes[]);
 void xyflush_c (int tno);
 void xyclose_c (int tno);
+int  xydim_c   (int tno);
 void xyread_c  (int tno, int index, float *array);
 void xywrite_c (int tno, int index, Const float *array);
 void xymkrd_c  (int tno, int index, int *runs, int n, int *nread);
@@ -274,6 +277,9 @@ void xyzwrite_c (int tno, Const int coords[], Const float *data, Const int *mask
 void xyzpixwr_c (int tno, int pixelnr, Const float *data, Const int *mask);
 void xyzprfwr_c (int tno, int profilenr, Const float *data, Const int *mask, Const int *ndata);
 void xyzplnwr_c (int tno, int planenr, Const float *data, Const int *mask, Const int *ndata);
+void xyzmkbuf_c ();
+void xyzdim_c   (int tno, int *naxis, int *dimsub);
+int xyzpix_c    (int tno, int dims);
 
 /* bug.c */
 
@@ -287,6 +293,7 @@ void scropen_c  (int *handle);
 void scrclose_c (int handle);
 void scrread_c  (int handle, float *buffer, off_t offset, size_t length);
 void scrwrite_c (int handle, Const float *buffer, off_t offset, size_t length);
+void scrrecsz_c (int handle, size_t recsize);
 
 /* key.c */
 
