8e30b1c3a1b248a7ad4f14020070b54833ba50b8
Mark Wieringa <Mark.Wieringa@csiro.au>
2009-04-24 05:05:50 +0000

    merge 1.2

--- subs/mp.h
+++ subs/mp.h
@@ -1,4 +1,2 @@
-	integer maxdig,maxvar
-	parameter(maxdig=8,maxvar=100)
-	integer first,maxint,base,vars(maxdig+1,maxvar)
-	common/mp/first,maxint,base,vars
+	integer mpBase,mpBase2,mpMaxInt(3)
+	common/mpcomm/mpBase,mpBase2,mpMaxInt
