55fb8eddd3cd69fbdd06eae7dc803515bae8a21c
mirmgr <mirmgr>
2009-08-31 02:43:37 +0000

    Merged revision 1.5.

--- inc/maxdim.h
+++ inc/maxdim.h
@@ -1,29 +1,39 @@
-c=======================================================================
-c - MAXBUF tells us how many words of memory we can use for data
-	INTEGER   MAXBUF
-        PARAMETER(MAXBUF=1000000)
-c-----------------------------------------------------------------------
-c - MAXDIM is an often used parameter, to indicate maximum size of maps
-	INTEGER   MAXDIM
-	PARAMETER(MAXDIM=16384)
-c-----------------------------------------------------------------------
-c		maximum number of antennae (HC=3/6/9/..., WSRT=14, VLA=27)
-	INTEGER   MAXANT
-	PARAMETER(MAXANT=64)
+C=======================================================================
+C $Id$
+C-----------------------------------------------------------------------
+C     Size of an INTEGER array used to implement a memory heap.  This
+C     array is the sole variable in blank COMMON in Miriad.  The default
+C     value allocates 4MiB (for normal 4-byte INTEGERs).
+      INTEGER   MAXBUF
+      PARAMETER(MAXBUF = 1024*1024)
 
-c		maximum number of baselines
-	INTEGER   MAXBASE
-	PARAMETER(MAXBASE=((MAXANT*(MAXANT+1))/2))
+C     Maximum image axis length.  Array dimensions are typically a few
+C     times MAXDIM (never MAXDIM**2) so MAXDIM is associated with a much
+C     smaller allocation of static memory than MAXBUF.  Thus the default
+C     value of MAXDIM is quite generous.  Values of MAXDIM > 32767 cause
+C     segvs in mfclean.  Note that, depending on the algorithm, MAXBUF
+C     may also play an important role in determining the maximum image
+C     size that can be handled.
+      INTEGER   MAXDIM
+      PARAMETER(MAXDIM = 16*1024)
 
-c		maximum number of channels in spectral data
-	INTEGER   MAXCHAN
-	PARAMETER(MAXCHAN=32772)
+C     Maximum number of antennas (SCAMP=96).
+      INTEGER   MAXANT
+      PARAMETER(MAXANT = 96)
 
-c		maximum number of windows in visibility data
-	INTEGER   MAXWIN
-	PARAMETER(MAXWIN=16)
-c
-c		maximum number of wideband channels.
-	INTEGER MAXWIDE
-	PARAMETER(MAXWIDE=18)
-c=======================================================================
+C     Maximum number of baselines, including autocorrelations.
+      INTEGER   MAXBASE
+      PARAMETER(MAXBASE = ((MAXANT*(MAXANT+1))/2))
+
+C     Maximum number of channels in spectral data.
+      INTEGER   MAXCHAN
+      PARAMETER(MAXCHAN = 32*1024 + 4)
+
+C     Maximum number of windows in visibility data.
+      INTEGER   MAXWIN
+      PARAMETER(MAXWIN = 16)
+
+C     Maximum number of wideband channels.
+      INTEGER   MAXWIDE
+      PARAMETER(MAXWIDE = 18)
+C=======================================================================
