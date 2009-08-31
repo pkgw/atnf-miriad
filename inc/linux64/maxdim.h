7aff8d2a56fbc59d63a60cc0e77dde7028379e44
Mark Calabretta <mcalabre@atnf.csiro.au>
2009-08-31 02:35:40 +0000

    Increased MAXANT to 96 for SCAMP by request of Mike Kesteven.

--- inc/linux64/maxdim.h
+++ inc/linux64/maxdim.h
@@ -1,45 +1,45 @@
-c=======================================================================
-c $Id$
-c-----------------------------------------------------------------------
-c     Size of an INTEGER array used to implement a memory heap.  This
-c     array is the sole variable in blank COMMON in Miriad.  Trial-and-
-c     error compilations on an x86-64 system with gcc/g77 show that the
-c     limit on MAXBUF for which most tasks build successfully is
-c     1073741823 (2**30 - 1) which corresponds to 4GiB.  With MAXDIM
-c     less than 32768 the limit for which all tasks build successfully
-c     is about 260000000; unsuccessful links produce messages about
-c     truncated relocations, imom being the worst offender.
-c     The default value allocates 128MiB (for normal 4-byte INTEGERs).
+C=======================================================================
+C $Id$
+C-----------------------------------------------------------------------
+C     Size of an INTEGER array used to implement a memory heap.  This
+C     array is the sole variable in blank COMMON in Miriad.  Trial-and-
+C     error compilations on an x86-64 system with gcc/g77 show that the
+C     limit on MAXBUF for which most tasks build successfully is
+C     1073741823 (2**30 - 1) which corresponds to 4GiB.  With MAXDIM
+C     less than 32768 the limit for which all tasks build successfully
+C     is about 260000000; unsuccessful links produce messages about
+C     truncated relocations, imom being the worst offender.
+C     The default value allocates 128MiB (for normal 4-byte INTEGERs).
       INTEGER   MAXBUF
-      PARAMETER(MAXBUF=  33554432)
+      PARAMETER(MAXBUF = 32*1024*1024)
 
-c     Maximum image axis length.  Array dimensions are typically a few
-c     times MAXDIM (never MAXDIM**2) so MAXDIM is associated with a much
-c     smaller allocation of static memory than MAXBUF.  Thus the default
-c     value of MAXDIM is quite generous.  Values of MAXDIM > 32767 cause
-c     segvs in mfclean.  Note that, depending on the algorithm, MAXBUF
-c     may also play an important role in determining the maximum image
-c     size that can be handled.
+C     Maximum image axis length.  Array dimensions are typically a few
+C     times MAXDIM (never MAXDIM**2) so MAXDIM is associated with a much
+C     smaller allocation of static memory than MAXBUF.  Thus the default
+C     value of MAXDIM is quite generous.  Values of MAXDIM > 32767 cause
+C     segvs in mfclean.  Note that, depending on the algorithm, MAXBUF
+C     may also play an important role in determining the maximum image
+C     size that can be handled.
       INTEGER   MAXDIM
-      PARAMETER(MAXDIM=16384)
+      PARAMETER(MAXDIM = 16*1024)
 
-c     Maximum number of antennae (HC=3/6/9/..., WSRT=14, VLA=27).
+C     Maximum number of antennas (SCAMP=96).
       INTEGER   MAXANT
-      PARAMETER(MAXANT=64)
+      PARAMETER(MAXANT = 96)
 
-c     Maximum number of baselines.
+C     Maximum number of baselines, including autocorrelations.
       INTEGER   MAXBASE
-      PARAMETER(MAXBASE=((MAXANT*(MAXANT+1))/2))
+      PARAMETER(MAXBASE = ((MAXANT*(MAXANT+1))/2))
 
-c     Maximum number of channels in spectral data.
+C     Maximum number of channels in spectral data.
       INTEGER   MAXCHAN
-      PARAMETER(MAXCHAN=70000)
+      PARAMETER(MAXCHAN = 70000)
 
-c     Maximum number of windows in visibility data.
+C     Maximum number of windows in visibility data.
       INTEGER   MAXWIN
-      PARAMETER(MAXWIN=16)
+      PARAMETER(MAXWIN = 16)
 
-c     Maximum number of wideband channels.
+C     Maximum number of wideband channels.
       INTEGER   MAXWIDE
-      PARAMETER(MAXWIDE=18)
-c=======================================================================
+      PARAMETER(MAXWIDE = 18)
+C=======================================================================
