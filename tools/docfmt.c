44a04068b75556cf5ac3a3c77436240a95bb74d4
Mark Calabretta <mcalabre@atnf.csiro.au>
2010-04-28 07:18:15 +0000

    Handle RCS revision information.

--- tools/docfmt.c
+++ tools/docfmt.c
@@ -1,33 +1,35 @@
-/************************************************************************/
-/*= docfmt - Format Miriad .doc files.					*/
-/*& rjs									*/
-/*: tools								*/
+/**********************************************************************/
+/*= docfmt - Format Miriad .doc files.                                */
+/*& rjs                                                               */
+/*: tools                                                             */
 /*+
   docfmt formats a Miriad .doc file into a more readable form.
-									*/
-/*--									*/
+                                                                      */
+/*$Id$ */
+/*--                                                                  */
 /*
   History:
-    25jun91 rjs	 Original version.
+    25jun91 rjs  Original version.
     16jul91 rjs  Changed output format to appease nebk, and added -k
-		 switch.
+                 switch.
      9aug93 rjs  Exit (rather than return) with 0 to appease VAXs.
     02jan07 rjs  Some standardizing.
-************************************************************************/
+***********************************************************************/
 
-#define VERSION "Docfmt: version 1.0 02-Jan-07"
-#define private static
 #include <stdio.h>
 #include <stdlib.h>
-#define MAXLINE 128
 
+#define VERSION "docfmt: Revision 1.4, 2010/04/28"
+#define private static
+#define MAXLINE 128
 #define TRUE 1
 #define FALSE 0
 
 private void process(),usage();
 private char *skip();
 
-/************************************************************************/
+/**********************************************************************/
+
 main(argc,argv)
 int argc;
 char *argv[];
@@ -36,55 +38,55 @@ char *argv[];
   int i,nin;
   FILE *infd,*outfd;
 
-/* Process the command line. */
-
+  /* Process the command line. */
   keyword = outfile = NULL;
   nin = 0;
-  for(i=1; i < argc; i++){
+  for(i=1; i < argc; i++) {
     s = argv[i];
 
-/* Flags and switches. */
-
-    if(*s == '-'){
+    /* Flags and switches. */
+    if (*s == '-') {
       argv[i] = NULL;
-      while(*++s)switch(*s){
-	case '?': usage(); exit(0);
-	case 'k': if(i < argc-1){
-		    keyword = argv[++i];
-		    argv[i] = NULL;
-		   }
-		   break;
-        default: fprintf(stderr,"Unrecognised flag %c\n",*s);
+      while (*++s) {
+        switch(*s) {
+        case '?':
+          usage();
+          exit(0);
+        case 'k':
+          if (i < argc-1) {
+            keyword = argv[++i];
+            argv[i] = NULL;
+          }
+          break;
+        default:
+          fprintf(stderr,"Unrecognised flag %c\n",*s);
+        }
       }
 
-/* An output file */
-
-    } else if(*s == '>') {
+    /* An output file */
+    } else if (*s == '>') {
       argv[i++] = NULL;
-      if(i < argc){
-	outfile = argv[i];
-	argv[i] = NULL;
+      if (i < argc) {
+        outfile = argv[i];
+        argv[i] = NULL;
       }
-
-/* An input file. */
 
     } else {
+      /* An input file. */
       nin++;
     }
   }
 
-/* Open the output file, if required. */
-
-  if(outfile == NULL) outfd = stdout;
+  /* Open the output file, if required. */
+  if (outfile == NULL) outfd = stdout;
   else outfd = fopen(outfile,"w");
-  if( outfd == NULL) { perror("open out"); exit(1); }
+  if ( outfd == NULL) { perror("open out"); exit(1); }
 
-/* Process each of the input files. */
-
-  if(nin > 0){
-    for(i=1; i<argc; i++) if(argv[i] != NULL){
+  /* Process each of the input files. */
+  if (nin > 0) {
+    for(i=1; i<argc; i++) if (argv[i] != NULL) {
       infd = fopen(argv[i],"r");
-      if(infd == NULL) { perror("open in"); exit(1); }
+      if (infd == NULL) { perror("open in"); exit(1); }
       process(infd,outfd,keyword);
       fclose(infd);
     }
@@ -93,15 +95,19 @@ char *argv[];
   }
   exit(0);
 }
+
 /**********************************************************************/
+
 private void usage()
 {
   fprintf(stderr,"%s\n",VERSION);
-  fprintf(stderr,"This formats a Miriad .doc file into a readable form\n\n");
+  fprintf(stderr,"Format a Miriad .doc file into a readable form\n\n");
   fprintf(stderr,"Usage:\n");
   fprintf(stderr,"docfmt [-?] [-k keyword] infile > outfile\n");
 }
+
 /**********************************************************************/
+
 private void process(fin,fout,keyword)
 FILE *fin,*fout;
 char *keyword;
@@ -110,53 +116,73 @@ char *keyword;
   (FILE *fout) a format version of it.
 
   Inputs:
-    fin		File descriptor of the input.
-    fout	File descriptor of the output.
+    fin         File descriptor of the input.
+    fout        File descriptor of the output.
 ----------------------------------------------------------------------*/
 {
   char line[MAXLINE],*s;
   int echo,length;
 
-/* Process the input file. */
-
+  /* Process the input file. */
   echo = keyword == NULL;
-  if(!echo)length = strlen(keyword);
+  if (!echo)length = strlen(keyword);
 
-  while(fgets(line,MAXLINE,fin) != NULL){
+  while (fgets(line,MAXLINE,fin) != NULL) {
+    /* If the line does not start with a percent, just write it out.
+       Otherwise do some processing. */
+    if (!echo && strncmp(line,"%A",2)) {
+      /* Nothing. */
 
-/* If the line does not start with a percent, just write it out. Otherwise
-   do some processing. */
+    } else if (*line != '%') {
+      fputs(line,fout);
 
-    if(!echo && strncmp(line,"%A",2));
-    else if(*line != '%')fputs(line,fout);
-    else switch(line[1]){
-      case 'N': fprintf(fout,"\nTask: %s",skip(line));		break;
-      case 'D': fprintf(fout,"Purpose: %s",skip(line));		break;
-      case ':': fprintf(fout,"Categories: %s",skip(line));	break;
-      case 'B': fprintf(fout,"\n");				break;
+    } else {
+      switch(line[1]) {
+      case 'N':
+        fprintf(fout,"\nTask: %s",skip(line));
+        break;
+      case 'D':
+        fprintf(fout,"Purpose: %s",skip(line));
+        break;
+      case ':':
+        fprintf(fout,"Categories: %s",skip(line));
+        break;
+      case 'B':
+        fprintf(fout,"\n");
+        break;
       case 'A':
-	s = skip(line);
-	echo = keyword == NULL;
-	if(!echo)echo = !strncmp(s,keyword,length);
-	if(echo)fprintf(fout,"\nKey: %s",skip(line));		break;
-      case 'P':							break;
-      default: fputs(line,fout);
+        s = skip(line);
+        echo = keyword == NULL;
+        if (!echo)echo = !strncmp(s,keyword,length);
+        if (echo)fprintf(fout,"\nKey: %s",skip(line));
+        break;
+      case 'P':
+        break;
+      case 'R':
+        fprintf(fout,"Revision: %s",skip(line));
+        break;
+      default:
+        fputs(line,fout);
+      }
     }
   }
 }
+
 /**********************************************************************/
+
 private char *skip(line)
 char *line;
 /*
-  This skips over the leading %x and blanks.
+  Skip over the leading %x and blanks.
+
   Input:
-    line	The input string.
+    line        The input string.
   Output:
-    skip	Points to the first non-blank character after the %x.
+    skip        Points to the first non-blank character after the %x.
 ----------------------------------------------------------------------*/
 {
   char *s;
   s = line + 2;
-  while(*s == ' ' || *s == '\t')s++;
+  while (*s == ' ' || *s == '\t') s++;
   return(s);
 }
