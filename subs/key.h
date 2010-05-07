861a00509645497214992584806f544dda36f884
Mark Calabretta <mcalabre@atnf.csiro.au>
2010-05-07 06:19:16 +0000

    Increased MAXLEN from 10000 to 32768.  The limit was hit by Tim Robishaw who was trying to use IMCOMB to stack 700 images.

--- subs/key.h
+++ subs/key.h
@@ -1,7 +1,9 @@
-	integer maxlen,maxkeys
-	parameter(maxlen=10000,maxkeys=32)
-	character pbuf*(maxlen),keys(maxkeys)*8
-	integer lu(maxkeys),k1(maxkeys),k2(maxkeys),nkeys
-	logical expanded(maxkeys),qhelp
-	common/keycommc/pbuf,keys
-	common/keycomm/ lu,k1,k2,expanded,nkeys,qhelp
+      integer MAXLEN, MAXKEYS
+      parameter(MAXLEN=32768, MAXKEYS=32)
+
+      logical expanded(MAXKEYS), luc(MAXKEYS), qhelp
+      integer k1(MAXKEYS), k2(MAXKEYS), lu(MAXKEYS), nkeys
+      character keys(MAXKEYS)*8, pbuf*(MAXLEN)
+
+      common /keycommc/ pbuf, keys
+      common /keycomm/  lu, k1, k2, luc, expanded, nkeys, qhelp
