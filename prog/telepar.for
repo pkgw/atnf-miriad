f90e00593657ce1cbdcb10742c2a69958920336a
Mark Calabretta <mcalabre@atnf.csiro.au>
2010-05-11 00:32:24 +0000

    Reestablished the change made at revision 1.4.  Added version reporting using VERSAN, removed IMPLICIT NONE, detabbified, fixed long lines.

--- prog/telepar.for
+++ prog/telepar.for
@@ -1,16 +1,18 @@
-c************************************************************************
-	program telepar
-	implicit none
+c***********************************************************************
+      program telepar
 c
-c= telepar -- Tell about telescope characteristics.
+c= telepar -- Report telescope characteristics.
 c& rjs
 c: utility
 c+
-c	TELEPAR gives the characteristics of various observatories.
-c	Its main use is to check that the characteristics are correct.
+c       TELEPAR reports the characteristics of various observatories.
+c       Its main use is to check that the characteristics are correct.
+c
 c@ telescop
-c	Name of the observatory. Several can be given. If none are
-c	given, TELEPAR simply lists the known observatories.
+c       Name of the observatory.  Several can be given.  If none are
+c       given, TELEPAR simply lists the known observatories.
+c
+c$Id$
 c--
 c  History:
 c    rjs  20jun91 Original version.
@@ -24,140 +26,141 @@ c    sdw  8jul06  Doesnt print all characteristics !
 c                 added subdiam,ew,nants,ellimt
 c                 also tried to improve formatting.
 c    rjs 23apr10  Reworked formatting was failing on some machines.
-c		  Rework the formatting again.
-c
-c $Id$
-c------------------------------------------------------------------------
-	character version*(*)
-	integer MAXOBS
-	parameter(version='Telepar: version 3.1 23-Apr-10')
-	parameter(MAXOBS=16)
-	include 'mirconst.h'
-	character string*20,observs(MAXOBS)*12,observ*12
-	logical ok
-	integer nobs,i,n
-	double precision value
-c
-	character rangle*20
-c
-	call output(version)
-	call keyini
-	call mkeya('telescop',observs,MAXOBS,nobs)
-	call keyfin
-c
-	if(nobs.eq.0)then
-	  call obsPrint
-	endif
-c
-	do i=1,nobs
-	  call output('********************************')
-	  observ = observs(i)
-	  call ucase(observ)
-c
-	  n = 0
-c
-	  call obspar(observ,'latitude',value,ok)
-c
-c         Put output here to get better output order
-	  call output('Observatory:           '//observ)
-	  if(ok)then
-	    n = n + 1
-	    string = rangle(value)
-	    call output('Latitude:              '//string
-     *                   //' deg:mm:ss.ss')
-	  endif
-c
-	  call obspar(observ,'longitude',value,ok)
-	  if(ok)then
-	    n = n + 1
-	    string = rangle(value)
-	    call output('Longitude:             '//string
-     *                  //' deg:mm:ss.ss')
-	  endif
-c
-	  call obspar(observ,'height',value,ok)
-	  if(ok)then
-	    n = n + 1
-            write(string,'(f8.2)') value
-	    call output('Height:              '//string
-     *                  //'   meters')	
-	  endif
-c
-	  call obspar(observ,'evector',value,ok)
-	  if(ok)then
-	    n = n + 1
-            value=180/pi*value
-            write(string,'(f8.2)') value
-	    call output('Feed Offset angle:   '//string
-     *                  //'   degrees')
-	  endif
-c
-	  call obspar(observ,'mount',value,ok)
-	  if(ok)then
-	    n = n + 1
-	    if(value.eq.0)string = 'Alt-az'
-	    if(value.eq.1)string = 'Equatorial'
-	    if(value.eq.3)string = 'XY-EW'
-	    if(value.eq.4)string = 'Nasmyth'
-	    call output('Mount:                 '//string)	
-	  endif
-c
-	  call obspar(observ,'antdiam',value,ok)
-	  if(ok)then
-	    n = n + 1
-            write(string,'(f8.2)') value
-	    call output('Antenna diameter:    '//string
-     *                  //'   meters')
-	  endif
-c
-	  call obspar(observ,'jyperk',value,ok)
-	  if(ok)then
-	    n = n + 1
-            write(string,'(f8.2)') value
-	    call output('System Gain:         '//string
-     *                  //'   Jy/K')
-	  endif
-c
-	  call obspar(observ,'subdiam',value,ok)
-	  if(ok)then
-	    n = n + 1
-            write(string,'(f8.2)') value
-	    call output('Subreflector diam:   '//string
-     *                  //'   meters')
-	  endif
-c
-	  call obspar(observ,'systemp',value,ok)
-	  if(ok)then
-	    n = n + 1
-            write(string,'(f8.2)') value
-	    call output('System Temp :        '//string
-     *                  //'   K')
-	  endif
-c
-	  call obspar(observ,'ellimit',value,ok)
-	  if(ok)then
-	    n = n + 1
-            value=180/pi*value
-            write(string,'(f8.2)') value
-	    call output('Elevation Limit:     '//string
-     *                  //'   degrees')
-	  endif
-c
-	  call obspar(observ,'nants',value,ok)
-	  if(ok)then
-	    n = n + 1
-            write(string,'(i4)') nint(value)
-	    call output('Number of Antennas:  '//string)
-	  endif
-c
-	  call obspar(observ,'ew',value,ok)
-	  if(ok)then
-	    n = n + 1
-            write(string,'(f6.2)') value
-	    call output('E-W Array:           '//string)
-	  endif
-c
-	  if(n.eq.0)call bug('w','No information was found')
-	end do
+c                 Rework the formatting again.
+c-----------------------------------------------------------------------
+      include 'mirconst.h'
 
-        end
+      integer MAXOBS
+      parameter(MAXOBS=16)
+
+      logical ok
+      integer i, n, nobs
+      double precision value
+      character observ*12, observs(MAXOBS)*12, string*20, version*80
+
+c     Externals.
+      character rangle*20, versan*80
+c-----------------------------------------------------------------------
+      version = versan ('telepar',
+     :                  '$Revision$',
+     :                  '$Date$')
+
+      call keyini
+      call mkeya('telescop',observs,MAXOBS,nobs)
+      call keyfin
+
+      if(nobs.eq.0)then
+        call obsPrint
+      endif
+
+      do i=1,nobs
+        call output('********************************')
+        observ = observs(i)
+        call ucase(observ)
+
+        n = 0
+
+        call obspar(observ,'latitude',value,ok)
+
+c       Put output here to get better output order
+        call output('Observatory:           '//observ)
+        if(ok)then
+          n = n + 1
+          string = rangle(value)
+          call output('Latitude:              '//string
+     *                //' deg:mm:ss.ss')
+        endif
+
+        call obspar(observ,'longitude',value,ok)
+        if(ok)then
+          n = n + 1
+          string = rangle(value)
+          call output('Longitude:             '//string
+     *                //' deg:mm:ss.ss')
+        endif
+
+        call obspar(observ,'height',value,ok)
+        if(ok)then
+          n = n + 1
+          write(string,'(f8.2)') value
+          call output('Height:              '//string
+     *                //'   metres')
+        endif
+
+        call obspar(observ,'evector',value,ok)
+        if(ok)then
+          n = n + 1
+          value=180/pi*value
+          write(string,'(f8.2)') value
+          call output('Feed Offset angle:   '//string//'   degrees')
+        endif
+
+        call obspar(observ,'mount',value,ok)
+        if(ok)then
+          n = n + 1
+          if(value.eq.0)string = 'Alt-az'
+          if(value.eq.1)string = 'Equatorial'
+          if(value.eq.3)string = 'XY-EW'
+          if(value.eq.4)string = 'Nasmyth'
+          call output('Mount:                 '//string)
+        endif
+
+        call obspar(observ,'antdiam',value,ok)
+        if(ok)then
+          n = n + 1
+          write(string,'(f8.2)') value
+          call output('Antenna diameter:    '//string
+     *                //'   metres')
+        endif
+
+        call obspar(observ,'jyperk',value,ok)
+        if(ok)then
+          n = n + 1
+            write(string,'(f8.2)') value
+          call output('System Gain:         '//string
+     *                //'   Jy/K')
+        endif
+
+        call obspar(observ,'subdiam',value,ok)
+        if(ok)then
+          n = n + 1
+            write(string,'(f8.2)') value
+          call output('Subreflector diam:   '//string
+     *                //'   metres')
+        endif
+
+        call obspar(observ,'systemp',value,ok)
+        if(ok)then
+          n = n + 1
+          write(string,'(f8.2)') value
+          call output('System Temp :        '//string
+     *                //'   K')
+        endif
+
+        call obspar(observ,'ellimit',value,ok)
+        if(ok)then
+          n = n + 1
+          value=180/pi*value
+          write(string,'(f8.2)') value
+          call output('Elevation Limit:     '//string
+     *                //'   degrees')
+        endif
+
+        call obspar(observ,'nants',value,ok)
+        if(ok)then
+          n = n + 1
+          write(string,'(i4)') nint(value)
+          call output('Number of Antennas:  '//string)
+        endif
+
+        call obspar(observ,'ew',value,ok)
+        if(ok)then
+          n = n + 1
+          write(string,'(f6.2)') value
+          call output('E-W Array:           '//string)
+        endif
+
+        if(n.eq.0)call bug('w','No information was found')
+      end do
+
+      end
