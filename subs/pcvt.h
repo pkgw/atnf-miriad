45e45db24776ab33ebd9d43c8d8a13e00a07f1d9
Mark Calabretta <mcalabre@atnf.csiro.au>
2010-07-28 06:28:04 +0000

    Renamed a couple of variables.

--- subs/pcvt.h
+++ subs/pcvt.h
@@ -1,5 +1,6 @@
-	integer ira,idec,naxis,coObj1,coObj2
-	double precision obstime
-	logical nop,dofk45z,dofk54z
-	common/pcvtcom/obstime,ira,idec,naxis,coObj1,coObj2,nop,
-     *	  dofk45z,dofk54z
+      logical   dofk45z, dofk54z
+      integer   coObj1, coObj2, galeq, ilat, ilng, naxis
+      double precision obstime
+
+      common /pcvtcom/ obstime, coObj1, coObj2, galeq, ilat, ilng,
+     *                 naxis, dofk45z, dofk54z
