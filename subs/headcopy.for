9c631d53fb2a1a44eb85d2d4120094b062691a65
Mark Calabretta <mcalabre@atnf.csiro.au>
2010-09-24 05:32:06 +0000

    Provide short-cuts for blc and trc.

--- subs/headcopy.for
+++ subs/headcopy.for
@@ -1,23 +1,26 @@
-c************************************************************************
 c* headcopy - Copy image header
 c& bpw
 c: utilities, image-i/o
 c+
-      subroutine headcopy(tIn, tOut, axnum, naxnum, blc, trc)
+      subroutine headcopy(lIn, lOut, axMap, nAxMap, blc, trc)
 
-      integer tIn, tOut, axnum(*), naxnum, blc(*), trc(*)
-
-c Headcopy copies the small items (the header) from one image to
-c another.  Its list contains all official header elements except for
-c the four described below.
+      integer lIn, lOut, axMap(*), nAxMap, blc(*), trc(*)
+c-----------------------------------------------------------------------
+c Headcopy copies the small items (header keywords) and the history from
+c one image to another.  It transfers all official header keywords with
+c only a few exceptions:
+c   - naxis and naxis# which are maintained by XYOPEN and XYZOPEN,
+c   - datamin, datamax, and rms must be recalculated for the output
+c     image and updated with WRHDR.
 c
 c The items listed in the KEYW array (below) are copied verbatim except
 c for crpix, crval, cdelt, crota and ctype which may be deleted,
-c exchanged, or reversed depending on axis permutations.  The axnum
-c array defines the relation between old and new axes, e.g.
+c exchanged, or reversed depending on axis permutations.  The axMap
+c array defines the relation between new and old axes in the sense
+c axMap(new) = old, e.g.
 c
-c                                     axnum
-c                       naxnum  (1)  (2)  (3)  (4)
+c                                     axMap
+c                       nAxMap  (1)  (2)  (3)  (4)
 c                       ------  ---  ---  ---  ---
 c           direct copy:   4     1    2    3    4
 c         delete z-axis:   2     1    2    0    0
@@ -30,7 +33,8 @@ c   The last entry shows the shorthand used in the common case where
 c   there are no axis permutations and the input and output images have
 c   the same dimensions (no sub-imaging).
 c
-c   For reversed axes cdelt is multiplied by -1 and 180 added to crota.
+c   For reversed axes cdelt is multiplied by -1 and 180 is added to
+c   crota.
 c
 c   For output images whose corners differ from the input image, crpix
 c   may have to be corrected.  This is done using the arrays blc and trc
@@ -39,22 +43,20 @@ c   numbers of the input image.  blc is used for non-reversed axes and
 c   trc for reversed axes.  Often one can use the output of subroutine
 c   boxinfo to get blc and trc.
 c
-c As xyopen and xyzopen maintain the naxis and naxis# items, they are
-c not copied here.  datamin, datamax, and rms are also not copied but
-c must be recalculated explicitly for the output image and updated with
-c wrhdr.
-c
 c   Input:
-c      tIn        Handle of input image.
-c      tOut       Handle of output image.
-c      axnum      Array giving relation betweem old and new axes
-c                 (negative values imply axis reversal).
-c      naxnum     Dimension of axnum array - ignored if axnum(1) = 0.
+c      lIn        Handle of input image.
+c      lOut       Handle of output image.
+c      axMap      Array that relates new axes with old (see above).
+c                 Can be specified as 0 (scalar) meaning verbatim copy.
+c      nAxMap     Dimension of axMap array - ignored if axMap(1) = 0.
 c      blc        List of bottom-left-corners of input image region.
-c      trc        List of top-right-corners   of input image region.
+c                 Can be specified as 0 (scalar) meaning (1,1,...).
+c      trc        List of top-right-corners of input image region.
+c                 Can be specified as 0 (scalar) meaning
+c                 (naxis1,naxis2,...) for the input image.
 c
 c $Id$
-c***********************************************************************
+c-----------------------------------------------------------------------
       integer   CRPIX, CDELT, CRVAL, CROTA, CTYPE
       parameter (CRPIX = 1, CDELT = 2, CRVAL = 3, CROTA = 4, CTYPE = 5)
 
@@ -62,7 +64,7 @@ c***********************************************************************
       parameter (NKEYS = 32)
 
       logical   verbtm
-      integer   iAxIn, iAxOut, nAxOut, k
+      integer   axLen, iAxIn, iAxOut, nAxOut, k
       double precision defVal(5), dvalue
       character avalue*80, keyIn*8, keyOut*8, keyw(NKEYS)*8
 
@@ -81,10 +83,10 @@ c***********************************************************************
      *    'vobs    ', 'history '/
 c-----------------------------------------------------------------------
 c     All axes in the output image must have coordinate keywords.  
-      call rdhdi(tOut, 'naxis', nAxOut, 0)
+      call rdhdi(lOut, 'naxis', nAxOut, 0)
 
 c     Loop for crpix, crval, cdelt, crota, and ctype.  
-      verbtm = axnum(1).eq.0
+      verbtm = axMap(1).eq.0
       do k = 1, 5
 c       Set default values.
         avalue = ' '
@@ -93,14 +95,14 @@ c       Set default values.
         do iAxOut = 1, nAxOut
           keyOut = keyw(k)(1:5) // itoaf(iAxOut)
 
-          if (verbtm .and. hdprsnt(tIn, keyOut)) then
+          if (verbtm .and. hdprsnt(lIn, keyOut)) then
 c           Copy verbatim.
-            call hdcopy(tIn, tOut, keyOut)
+            call hdcopy(lIn, lOut, keyOut)
 
           else
 c           Handle axis permutations.
-            if (.not.verbtm .and. iAxOut.le.naxnum) then
-              iAxIn = axnum(iAxOut)
+            if (.not.verbtm .and. iAxOut.le.nAxMap) then
+              iAxIn = axMap(iAxOut)
             else
 c             Use default values.
               iAxIn = 0
@@ -109,25 +111,37 @@ c             Use default values.
             if (iAxIn.ne.0) then
               keyIn = keyw(k)(1:5) // itoaf(abs(iAxIn))
 
-              if (hdprsnt(tIn, keyIn)) then
+              if (hdprsnt(lIn, keyIn)) then
 c               Read it from the input image.
                 if (k.eq.CTYPE) then
-                  call rdhda(tIn, keyIn, avalue, ' ')
+                  call rdhda(lIn, keyIn, avalue, ' ')
                 else
-                  call rdhdd(tIn, keyIn, dvalue, 0d0)
+                  call rdhdd(lIn, keyIn, dvalue, 0d0)
                 endif
 
                 if (iAxIn.gt.0) then
                   if (k.eq.CRPIX) then
 c                   Sub-imaging.
-                    dvalue = dvalue - blc(iAxIn) + 1d0
+                    if (blc(1).ne.0) then
+                      dvalue = dvalue - dble(blc(iAxIn)-1)
+                    endif
                   endif
 
                 else if (iAxIn.lt.0) then
 c                 Axis reversal.
+                  iAxIn = -iAxIn
+
                   if (k.eq.CRPIX) then
 c                   Sub-imaging.
-                    dvalue = trc(-iAxIn) - dvalue + 1d0
+                    if (trc(1).ne.0) then
+                      axLen = trc(iAxIn)
+                    else
+                      keyIn = 'naxis' // itoaf(iAxIn)
+                      call rdhdi(lOut, keyIn, axLen, 0)
+                    endif
+
+                    dvalue = dble(axLen+1) - dvalue
+
                   else if (k.eq.CDELT) then
                     dvalue = -dvalue
                   else if (k.eq.CROTA) then
@@ -139,9 +153,9 @@ c                   Sub-imaging.
 
 c           Write it to the output image.
             if (k.eq.CTYPE) then
-              call wrhda(tOut, keyOut, avalue)
+              call wrhda(lOut, keyOut, avalue)
             else
-              call wrhdd(tOut, keyOut, dvalue)
+              call wrhdd(lOut, keyOut, dvalue)
             endif
           endif
         enddo
@@ -149,7 +163,7 @@ c           Write it to the output image.
 
 c     Copy the remaining items verbatim.
       do k = 6, NKEYS
-         call hdcopy(tIn, tOut, keyw(k))
+         call hdcopy(lIn, lOut, keyw(k))
       enddo
 
       return
