03417cc3497d2c7aaea175da12006566167b8d9d
Mark Calabretta <mcalabre@atnf.csiro.au>
2010-10-14 03:51:24 +0000

    mirf'd+.

--- subs/hdtab.for
+++ subs/hdtab.for
@@ -14,8 +14,9 @@ c                 best.
 c
 c $Id$
 c***********************************************************************
+
       subroutine HdInit(mfs1,mosaic1)
-c
+
       logical mfs1,mosaic1
 c-----------------------------------------------------------------------
       include 'hdtab.h'
@@ -29,10 +30,10 @@ c-----------------------------------------------------------------------
 c***********************************************************************
 
       subroutine HdChk(tno,uvw)
-c
+
       integer tno
       double precision uvw(3)
-c
+c-----------------------------------------------------------------------
 c  Check that the header is the same.
 c-----------------------------------------------------------------------
       include 'hdtab.h'
@@ -45,116 +46,112 @@ c-----------------------------------------------------------------------
       double precision t1,dv,v0,epsi,r0,line(6),dtemp
       character s*16
 c-----------------------------------------------------------------------
-c
-c  Get all the values we can possibly want when we see a file for the
-c  first time.
-c
-      if(doinit)then
+c     Get all the values we can possibly want when we see a file for the
+c     first time.
+      if (doinit) then
         call HdFirst(tno)
         doinit = .false.
       endif
-      if(Count.eq.0)then
+
+      if (count.eq.0) then
         mcount = min(2*mcount+1,MAXCOUNT)
-c
-c  Form an average time and observatory velocity.
-c
-        call uvrdvrd(tno,'time',dtemp,0.d0)
+
+c       Form an average time and observatory velocity.
+        call uvrdvrd(tno,'time',dtemp,0d0)
         obstime = obstime + dtemp
         call uvrdvrr(tno,'vsource',vsource,0.0)
         call uvrdvrr(tno,'veldop',rtemp,0.0)
-        vobs = vobs  + rtemp - vsource
+        vobs  = vobs  + rtemp - vsource
         naver = naver + 1
-c
-c  Check for a change in the spectral axis.
-c
-        if(ctype3(1:4).eq.'VELO')then
+
+c       Check for a change in the spectral axis.
+        if (ctype3(1:4).eq.'VELO') then
           call uvinfo(tno,'line',line)
           nchan = nint(line(2))
-          if(.not.RChange)then
+          if (.not.rChange) then
             call uvfit1(tno,'restfreq',nchan,r0,epsi)
-            RConst = RConst.and.epsi.lt.0.001*restfreq
-            RChange = abs(restfreq-r0).gt.0.001*restfreq
+            rConst  = rConst .and. epsi.lt.restfreq*1d-3
+            rChange = abs(restfreq-r0).gt.restfreq*1d-3
           endif
-          if(.not.VChange)then
-            if(nchan.eq.1)then
+          if (.not.vChange) then
+            if (nchan.eq.1) then
               call uvinfo(tno,'velocity',v0)
               call uvinfo(tno,'bandwidth',dv)
-              dv = 0.001 * CMKS * dv / restfreq
+              dv = DCMKS * (dv / restfreq) * 1d-3
             else
               call uvfit2(tno,'velocity',nchan,dv,v0,epsi)
-              VLinear = VLinear.and.(epsi.lt.0.1*abs(cdelt3))
+              VLinear = VLinear .and. (epsi.lt.0.1d0*abs(cdelt3))
             endif
-            VChange = max(abs(crval3-v0),abs(cdelt3-dv)).gt.
-     *                                        0.1*abs(cdelt3)
+            vChange = max(abs(crval3-v0),abs(cdelt3-dv)).gt.
+     *                                        0.1d0*abs(cdelt3)
           endif
         endif
-c
-c  Check for a change in RA.
-c
-        if(.not.XChange.and..not.mosaic)then
-          call uvrdvrd(tno,'dec',t1,0.d0)
+
+c       Check for a change in RA.
+        if (.not.xChange .and. .not.mosaic) then
+          call uvrdvrd(tno,'dec',t1,0d0)
           call uvrdvrr(tno,'dra',t2,0.0)
           t2 = t2 / cos(t1)
-          call uvrdvrd(tno,'ra',t1,0.d0)
+          call uvrdvrd(tno,'ra',t1,0d0)
           t1 = t1 + t2
-          XChange = abs(crval1-t1).gt.TOL
+          xChange = abs(crval1-t1).gt.TOL
         endif
-c
-c  Check for a change in DEC.
-c
-        call uvrdvrd(tno,'dec',t1,0.d0)
+
+c       Check for a change in DEC.
+        call uvrdvrd(tno,'dec',t1,0d0)
         call uvrdvrr(tno,'ddec',t2,0.0)
         t1 = t1 + t2
-        if(.not.YChange.and..not.mosaic)then
-          YChange = abs(crval2-t1).gt.TOL
+        if (.not.yChange .and. .not.mosaic) then
+          yChange = abs(crval2-t1).gt.TOL
         endif
-c
-c  Accumulate info to determine whether its an e-w array.
-c
+
+c       Accumulate info to determine whether its an e-w array.
         sumlumv = sumlumv + (cos(t1)*uvw(2)+sin(t1)*uvw(3))**2
         sumuuvv = sumuuvv + uvw(2)*uvw(2) + uvw(3)*uvw(3)
-c
-c  Check for a change in source name.
-c
-        if(.not.SChange)then
+
+c       Check for a change in source name.
+        if (.not.sChange) then
           call uvrdvra(tno,'source',s,source)
-          SChange = s.ne.source
+          sChange = s.ne.source
         endif
       endif
-c
+
       Count = mod(Count+1,mcount)
+
       end
+
 c***********************************************************************
+
       subroutine HdFirst(tno)
-c
+
       integer tno
 c-----------------------------------------------------------------------
       integer CHANNEL,WIDE,VELOCITY
-      parameter(CHANNEL=1,WIDE=2,VELOCITY=3)
+      parameter (CHANNEL=1,WIDE=2,VELOCITY=3)
+
       include 'hdtab.h'
       include 'mirconst.h'
+
       double precision line(6),epsi
       real dra,ddec
       integer itype
-c
-c  Initialise all the logicals to check whether things are consistent.
-c
-      VChange = .false.
-      VLinear = .true.
-      Rconst  = .true.
-      RChange = .false.
-      XChange = .false.
-      YChange = .false.
-      SChange = .false.
-      Count = 0
-      mcount = 0
-c
-c  Handle the first two axes.
-c
-      if(.not.mosaic)then
-        call uvrdvrd(tno,'ra',crval1,0.d0)
-        call uvrdvrd(tno,'dec',crval2,0.d0)
-        call uvrdvrr(tno,'dra',dra,0.0)
+c-----------------------------------------------------------------------
+c     Initialise logicals used to check whether things are consistent.
+      rConst  = .true.
+      rChange = .false.
+      vChange = .false.
+      vLinear = .true.
+      sChange = .false.
+      xChange = .false.
+      yChange = .false.
+      count   = 0
+      mcount  = 0
+
+c     Handle the first two axes.
+      if (.not.mosaic) then
+        call uvrdvrd(tno,'ra', crval1,0d0)
+        call uvrdvrd(tno,'dec',crval2,0d0)
+        call uvrdvrr(tno,'dra', dra, 0.0)
         call uvrdvrr(tno,'ddec',ddec,0.0)
         crval1 = crval1 + dra/cos(crval2)
         crval2 = crval2 + ddec
@@ -163,31 +160,28 @@ c
       endif
       call pbRead(tno,pbtype)
       call uvrdvrr(tno,'epoch',epoch,1950.0)
-c
-c  Miscellaneous.
-c
+
+c     Miscellaneous.
       call uvrdvra(tno,'source',source,' ')
       call uvrdvra(tno,'telescop',telescop,' ')
       call uvrdvra(tno,'observer',observer,' ')
-c
-c  Initialise.
-c
-      sumuuvv = 0
-      sumlumv = 0
-      vobs = 0
-      obstime = 0
-      naver = 0
-c
-c  Handle the spectral axis.
-c
+
+c     Initialise.
+      sumuuvv = 0.0
+      sumlumv = 0.0
+      vobs    = 0.0
+      obstime = 0.0
+      naver   = 0
+
+c     Handle the spectral axis.
       call uvrdvra(tno,'veltype',ctype3,' ')
       call uvinfo(tno,'line',line)
       itype = nint(line(1))
-      if(itype.eq.CHANNEL)then
+      if (itype.eq.CHANNEL) then
         ltype = 'channel'
-      else if(itype.eq.WIDE)then
+      else if (itype.eq.WIDE) then
         ltype = 'wide'
-      else if(itype.eq.VELOCITY)then
+      else if (itype.eq.VELOCITY) then
         ltype = 'velocity'
       else
         call bug('f','Unrecognised line type')
@@ -196,53 +190,59 @@ c
       lstart = line(3)
       lwidth = line(4)
       lstep  = line(5)
-c
+
       call uvfit1(tno,'bandwidth',nchan,cdelt3,epsi)
-      if(ltype.ne.'wide')then
+      if (ltype.ne.'wide') then
         call uvfit1(tno,'restfreq',nchan,restfreq,epsi)
-        RConst = epsi.le.0.001*restfreq
+        rConst = epsi.le.restfreq*1d-3
       else
-        restfreq = 0
+        restfreq = 0d0
       endif
-c
-      if(mfs)then
+
+      if (mfs) then
         ctype3(1:4) = 'FREQ'
-      else if(restfreq.eq.0)then
-        if(nchan.gt.1)then
+      else if (restfreq.eq.0) then
+        if (nchan.gt.1) then
           call uvfit2(tno,'sfreq',nchan,cdelt3,crval3,epsi)
-          VLinear = epsi.le.0.1*abs(cdelt3)
+          VLinear = epsi.le.0.1d0*abs(cdelt3)
         else
           call uvinfo(tno,'sfreq',crval3)
         endif
         ctype3(1:4) = 'FREQ'
       else
-        if(nchan.gt.1)then
+        if (nchan.gt.1) then
           call uvfit2(tno,'velocity',nchan,cdelt3,crval3,epsi)
-          VLinear = epsi.le.0.1*abs(cdelt3)
+          VLinear = epsi.le.0.1d0*abs(cdelt3)
         else
           call uvinfo(tno,'velocity',crval3)
-          cdelt3 = 0.001 * CMKS * cdelt3 / restfreq
+          cdelt3 = DCMKS * (cdelt3 / restfreq) * 1d-3
         endif
         ctype3(1:4) = 'VELO'
       endif
-c
+
       end
+
 c***********************************************************************
+
       subroutine HdDone(tno)
-c
+
       integer tno
 c-----------------------------------------------------------------------
       include 'hdtab.h'
-      Count = 0
+c-----------------------------------------------------------------------
+      Count  = 0
       mcount = 1
+
       end
+
 c***********************************************************************
+
       subroutine HdSet(dosin,cellx,celly,ra0,dec0,freq0)
-c
+
       logical dosin
       real    cellx,celly,freq0
       double precision ra0,dec0
-c
+c-----------------------------------------------------------------------
 c  Set remaining hdtab variables and do some basic checks.
 c
 c  Input:
@@ -277,60 +277,62 @@ c-----------------------------------------------------------------------
         ctype1 = 'RA---NCP'
         ctype2 = 'DEC--NCP'
       endif
-c
-c  Check for things that have changed.
-c
-      if (.not.RConst) call bug('w',
+
+c     Check for things that have changed.
+      if (.not.rConst) call bug('w',
      *    'Rest frequencies varied between channels by > 0.1%')
-      if (RChange) call bug('w',
+      if (rChange) call bug('w',
      *    'Rest frequencies varied by > 0.1% while reading data')
-      if (.not.VLinear) call bug('w',
+      if (.not.vLinear) call bug('w',
      *    'Channel frequencies/velocities deviated by > 10% from '//
      *    'linearity')
-      if (VChange) call bug('w',
+      if (vChange) call bug('w',
      *    'Channel velocities varied by > 10% while reading data')
-      if (XChange) call bug('w',
+      if (xChange) call bug('w',
      *    'The source RA changed by > 1 arcsec while reading data')
-      if (YChange) call bug('w',
+      if (yChange) call bug('w',
      *    'The source DEC changed by > 1 arcsec while reading data')
 
       end
+
 c***********************************************************************
+
       subroutine HdCoObj(coObj)
-c
+
       integer coObj
-c
+c-----------------------------------------------------------------------
 c  Generate a coordinate object.
 c-----------------------------------------------------------------------
       include 'hdtab.h'
-c
+c-----------------------------------------------------------------------
       call coCreate(coObj)
-      call coAxSet(coObj,1,ctype1,0.d0,crval1,cdelt1)
-      call coAxSet(coObj,2,ctype2,0.d0,crval2,cdelt2)
-      call coAxSet(coObj,3,ctype3,1.d0,crval3,cdelt3)
+      call coAxSet(coObj,1,ctype1,0d0,crval1,cdelt1)
+      call coAxSet(coObj,2,ctype2,0d0,crval2,cdelt2)
+      call coAxSet(coObj,3,ctype3,1d0,crval3,cdelt3)
       call coSetd(coObj,'vobs',dble(vobs/naver))
-      if(restfreq.ne.0)call coSetd(coObj,'restfreq',restfreq)
+      if (restfreq.ne.0) call coSetd(coObj,'restfreq',restfreq)
       call coSetd(coObj,'obstime',obstime/naver)
       call coSetd(coObj,'epoch',dble(epoch))
-      if(nchan.eq.1.or.mfs)call coSeta(coObj,'cellscal','CONSTANT')
+      if (nchan.eq.1 .or. mfs) call coSeta(coObj,'cellscal','CONSTANT')
       call coReinit(coObj)
-c
+
       end
+
 c***********************************************************************
+
       subroutine HdDefSiz(nx,ny)
-c
+
       integer nx,ny
-c
+c-----------------------------------------------------------------------
 c  Determine the default size of an image.
 c-----------------------------------------------------------------------
       include 'mirconst.h'
       include 'hdtab.h'
       real fwhm,cutoff,maxrad
       integer coObj,pbObj
-c
-c  Determine the FWHM of the telescope.
-c
-      if(pbtype.eq.' '.or.pbtype.eq.'SINGLE')then
+c-----------------------------------------------------------------------
+c     Determine the FWHM of the telescope.
+      if (pbtype.eq.' ' .or. pbtype.eq.'SINGLE') then
         call bug('f',
      *    'Unknown telescope -- cannot determine default image size')
       else
@@ -340,39 +342,41 @@ c
         call pbFin(pbObj)
         call coFin(coObj)
       endif
-c
+
       nx = max(nint(abs(fwhm/cdelt1)),1)
       ny = max(nint(abs(fwhm/cdelt2)),1)
+
       end
+
 c***********************************************************************
+
       subroutine HdWrite(tno,rms,nx,ny)
-c
+
       integer tno
       real rms
       integer nx,ny
-c
+c-----------------------------------------------------------------------
 c  Write all the header rubbish out to an image dataset.
-c
 c-----------------------------------------------------------------------
       include 'hdtab.h'
-c
+c-----------------------------------------------------------------------
       call wrhda(tno,'ltype', ltype)
       call wrhdr(tno,'lstart',lstart)
       call wrhdr(tno,'lwidth',lwidth)
       call wrhdr(tno,'lstep', lstep)
-c
-      if(telescop.ne.' ')call wrhda(tno,'telescop',telescop)
-      if(source.ne.' ')  call wrhda(tno,'object',  source)
-      if(observer.ne.' ')call wrhda(tno,'observer',observer)
-      if(rms.gt.0)call wrhdr(tno,'rms',rms)
-      if(.not.mosaic)then
-        if(obsra.ne.crval1.or.obsdec.ne.crval2)then
+
+      if (telescop.ne.' ') call wrhda(tno,'telescop',telescop)
+      if (source.ne.' ')  call wrhda(tno,'object',  source)
+      if (observer.ne.' ') call wrhda(tno,'observer',observer)
+      if (rms.gt.0) call wrhdr(tno,'rms',rms)
+      if (.not.mosaic) then
+        if (obsra.ne.crval1 .or. obsdec.ne.crval2) then
           call mosInit(nx,ny)
           call mosSet(1,obsra,obsdec,rms,pbtype)
           call mosSave(tno)
-        else if(pbtype.ne.telescop)then
+        else if (pbtype.ne.telescop) then
           call wrhda(tno,'pbtype',pbtype)
         endif
       endif
-c
+
       end
