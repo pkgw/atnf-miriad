2e5637e915aa4f4eaaefb87926a3a4ecf5627184
Mark Calabretta <mcalabre@atnf.csiro.au>
2011-02-14 06:42:13 +0000

    mirf'd.

--- subs/title.for
+++ subs/title.for
@@ -1,59 +1,67 @@
+c***********************************************************************
 c* Title -  Write title in standard format into LogFile.
 c& mchw
 c: image analysis,log-file
 c+
-	Subroutine Title(lIn,naxis,blc,trc,cbof)
-c
-	implicit none
-	integer lIn,naxis,blc(naxis),trc(naxis)
-c
-c   Write title in standard format into LogFile.
+      subroutine Title(lIn,naxis,blc,trc,cbof)
+
+      integer lIn,naxis,blc(naxis),trc(naxis)
+c  ---------------------------------------------------------------------
+c  Write title in standard format into LogFile.
 c
 c  Inputs:
-c    lIn	The handle of the Image.
-c    naxis	Number of axes of the Image.
-c    blc,trc	Corners of region of interest.
+c    lIn        The handle of the Image.
+c    naxis      Number of axes of the Image.
+c    blc,trc    Corners of region of interest.
 c  Output:
-c    cbof	Beam oversampling factor.
-c--
-c			mchw june 1990
-c-------------------------------------------------------------------c
-	double precision pi,rts
-	parameter(pi=3.141592654,rts=3600.d0*180.d0/pi)
-	integer length,lblc,ltrc
-	character line*80
-	character*9 bunit,ctype1,ctype2
-	character*20 txtblc,txttrc
-	real bmaj,bmin,restfreq,omega,cbof,DperJy
-	logical more
-c
-c  Get beam size etc.
-c
-	call GetBeam(lIn,naxis,bunit,bmaj,bmin,omega,cbof)
-	call rdhdr(lIn,'restfreq',restfreq,0.)
-	DperJy=1.
-	if(restfreq.ne.0.) then
-	  DperJy= (0.3/restfreq)**2 /(2.*1.38e3*omega)
-	endif
-c
-	write(line,'(a,a,a,f10.6,a,f10.4)') '  bunit: ',bunit,
-     *		'  restfreq: ',restfreq,'    K/Jy: ',DperJy
-	length=9+10+12+10+10+10
-	call LogWrite(line(1:length),more)
-c
-	call rdhda(lIn,'ctype1',ctype1,' ')
-	call rdhda(lIn,'ctype2',ctype2,' ')
-	write(line,'(a,a,a,a,a,f6.2,a,f6.2,a)')
-     *	  '  Axes: ',ctype1,' x ',ctype2,
-     *    '   Beam: ',bmaj*rts,' x ',bmin*rts,' arcsecs'
-	length = 8 + len(ctype1) + 3 + len(ctype2) + 9+6+3+6+8
-	call LogWrite(line(1:length),more)
-	call mitoaf(blc,3,txtblc,lblc)
-	call mitoaf(trc,3,txttrc,ltrc)
-	line = '  Bounding region is Blc=('//txtblc(1:lblc)//
-     *				       '),Trc=('//txttrc(1:ltrc)//')'
-	length = 26 + lblc + 7 + ltrc + 1
-	call LogWrite(line(1:length),more)
-	end
-c********1*********2*********3*********4*********5*********6*********7*c
+c    cbof       Beam oversampling factor.
+c-----------------------------------------------------------------------
+      include 'mirconst.h'
 
+      double precision RTS
+      parameter (RTS=3600d0*180d0/PI)
+
+      logical   more
+      integer   lblc, ltrc
+      real      bmaj, bmin, cbof, DperJy, freq, omega
+      character bunit*20, ctype1*9, ctype2*9, ctype3*9, line*80,
+     *          txtblc*20, txttrc*20
+
+      external  len1
+      integer   len1
+c-----------------------------------------------------------------------
+c     Get beam size etc.
+      call GetBeam(lIn,naxis,bunit,bmaj,bmin,omega,cbof)
+      call rdhdr(lIn,'restfreq',freq,0.0)
+      if (freq.eq.0.0) then
+        call rdhda(lIn,'ctype3',ctype3,' ')
+        if (ctype3(1:4).eq.'FREQ') then
+          call rdhdr(lIn,'crval3',freq,0.0)
+        endif
+      endif
+
+      DperJy=1.0
+      if (freq.ne.0.0) then
+        DperJy= (0.3/freq)**2 /(2.0*1.38e3*omega)
+      endif
+
+      write(line,'(a,a,a,f10.6,a,f15.4)')
+     *      '  bunit: ', bunit(1:len1(bunit)),
+     *      '  frequency: ', freq,
+     *      '    K/Jy: ', DperJy
+      call LogWrite(line,more)
+
+      call rdhda(lIn,'ctype1',ctype1,' ')
+      call rdhda(lIn,'ctype2',ctype2,' ')
+      write(line,'(a,a,a,a,a,f6.2,a,f6.2,a)')
+     *  '  Axes: ',ctype1,' x ',ctype2,
+     *  '   Beam: ',bmaj*RTS,' x ',bmin*RTS,' arcsecs'
+      call LogWrite(line,more)
+
+      call mitoaf(blc,3,txtblc,lblc)
+      call mitoaf(trc,3,txttrc,ltrc)
+      line = '  Bounding region is Blc=('//txtblc(1:lblc)//
+     *                          '),Trc=('//txttrc(1:ltrc)//')'
+      call LogWrite(line,more)
+
+      end
