e3e7bc52efc1dc73c058e57676019be0e4773fa2
Mark Calabretta <mcalabre@atnf.csiro.au>
2011-02-18 04:35:02 +0000

    Use coSpcSet in place of coVelSet.

--- subs/getfreq.for
+++ subs/getfreq.for
@@ -2,25 +2,23 @@ c*GetFreq -- Return frequency & increment for given image spectral pixel
 c& nebk
 c: utilities
 c+
-        subroutine getfreq (tin, pix, ifax, freq, finc, ierr)
+        subroutine getfreq (tin, pix, ifrq, freq, finc, ierr)
 
-        integer   tin, ifax, ierr
+        integer   tin, ifrq, ierr
         real      pix
         double precision freq, finc
 c  ---------------------------------------------------------------------
-c  Work out the frequency of a spectral pixel, whether the spectral
-c  axis is frequency or velocity, and regardless of which axis
-c  is the spectral axis.  Works for images only.
+c  Return the frequency of a spectral pixel, regardless of spectral axis
+c  type, or which axis is the spectral axis.  Works for images only.
 c
 c  Inputs:
 c    tin    i   Handle of dataset
 c    pix    r   Pixel of interest
-c  Input/output
-c    ifax   i   The spectral axis number.  If 0 on input, GETFREQ
-c               will look for it.
+c
 c  Output:
-c    freq   dp  Frequency in GHz.
-c    finc   dp  Frequency increment in GHz.
+c    ifrq   i   The spectral axis number.
+c    freq   d   Frequency in GHz.
+c    finc   d   Frequency increment in GHz.
 c    ierr   i   0 -> OK,
 c               1 -> no spectral axis,
 c History:
@@ -29,40 +27,37 @@ c
 c $Id$
 c-----------------------------------------------------------------------
       double precision freq1, freq2
-      character line*80
+      character algo*3, line*80
 c-----------------------------------------------------------------------
-c     Initialize
-      call coinit(tin)
-      ierr = 0
+c     Initialize the coordinate object.
+      call coInit(tin)
 
-c     Find spectral axis
-      if (ifax.le.0) then
-        call cofindax(tin, 'spectral', ifax)
-        if (ifax.eq.0) then
-          call cofin(tin)
-          call bug('w',
-     *     'GETFREQ: No spectral axis; could not work out frequency')
-          freq = 0.0
-          finc = 0.0
-          ierr = 1
-          return
-        endif
+c     Switch spectral axis to frequency.
+      call coSpcSet(tin, 'FREQ', ifrq, algo)
+      if (ifrq.eq.0) then
+        call coFin(tin)
+        call bug('w',
+     *   'GETFREQ: No spectral axis; could not work out frequency')
+        freq = 0d0
+        finc = 0d0
+        ierr = 1
+        return
       endif
 
-c     Set frequency axis and load conversion arrays
-      call covelset(tin, 'frequency')
-
-      call cocvt1(tin, ifax, 'ap', dble(pix-0.5), 'aw', freq1)
-      call cocvt1(tin, ifax, 'ap', dble(pix+0.5), 'aw', freq2)
-      call cocvt1(tin, ifax, 'ap', dble(pix),     'aw', freq)
-      call cofin(tin)
-
-      finc = freq2 - freq1
-
-      if (freq.le.0.0) then
-        write(line, 100) freq
-100     format('GETFREQ: ', f9.5, ' GHz is an unusual frequency')
+c     Compute the frequency.
+      call coCvt1(tin, ifrq, 'ap', dble(pix), 'aw', freq)
+      if (freq.le.0d0) then
+        write(line, 10) freq
+ 10     format('GETFREQ: ',f9.5,' GHz is an unusual frequency')
         call bug('w', line)
       endif
+
+c     Compute the frequency increment.
+      call coCvt1(tin, ifrq, 'ap', dble(pix-0.5), 'aw', freq1)
+      call coCvt1(tin, ifrq, 'ap', dble(pix+0.5), 'aw', freq2)
+      finc = freq2 - freq1
+
+      call coFin(tin)
+      ierr = 0
 
       end
