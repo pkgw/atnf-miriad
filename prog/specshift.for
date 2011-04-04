ba03ebc00e204464c116c23cafc806fd958e6030
Mark Calabretta <mcalabre@atnf.csiro.au>
2011-04-04 04:55:51 +0000

    mirf'd and combined the three trivial subroutines into one main program. Added version reporting using VERSAN.

--- prog/specshift.for
+++ prog/specshift.for
@@ -1,245 +1,215 @@
+      program specshift
+
 c= SPECSHIFT - shift velocity profiles
 c& tw
 c: analysis
 c+
-c   SPECSHIFT shifts all spectra in a cube according to a 2-D template
-c   (a velocity field).  The velocity of the template is made the
-c   central pixel of the spectrum.  If the template is blanked or out
-c   of range the spectrum is unchanged.  Based on Bart Wakker's HANNING.
+c       SPECSHIFT shifts all spectra in a cube according to a 2-D
+c       template (a velocity field).  The velocity of the template is
+c       made the central pixel of the spectrum.  If the template is
+c       blanked or out-of-range the spectrum is unchanged.  Based on
+c       Bart Wakker's HANNING.
 c
 c@ in
-c        The input data cube. vxy and xyv images are acceptable inputs.
-c        No default.
-c@ region
-c        The region of the input cube to modify.  By default the whole
-c        cube is processed.
+c        The input data cube.  No default.
+c
 c@ tin
 c        The template image, should be 2-D with same RA/DEC axes as the
 c        input cube.
+c
+c@ out
+c        The output image.  No default.
+c
+c@ region
+c        The region of the input cube to modify.  Default is the whole
+c        cube.
+c
 c@ refpix
 c        The velocity pixel which will be assigned to the template
 c        velocity.  Default is the middle pixel of the v-range.
+c
 c@ options
-c         doblank    Don't wrap profile around; blank if out of range.
-c                    By default the edge channels are assumed to be
-c                    noise so the profile is wrapped when shifting
-c                    beyond edges.
-c@ out
-c        The output image.
+c        doblank    Don't wrap profile around; blank if out of range.
+c                   By default the edge channels are assumed to be
+c                   noise so the profile is wrapped when shifting
+c                   beyond edges.
 c--
-c
 c   History:
-c
-c    tw   26mar06  Created.
-c    tw   22jun07  doblank option.
-c
+c    Refer to the RCS log, v1.1 includes prior revision information.
 c-----------------------------------------------------------------------
-       program specshift
+      include 'maxdim.h'
+      include 'maxnax.h'
 
-       character*80 version
-       parameter (version = 'specshift: version 1.0 22jun07')
+      integer   MAXBOXES, NOPTS
+      parameter (MAXBOXES=1024, NOPTS=1)
 
-       include      'maxnax.h'
+      logical   doblank, flags(MAXDIM), mask(MAXDIM), outrange,
+     *          present(NOPTS), workmsk(MAXDIM)
+      integer   axlen(MAXNAX), boxes(MAXBOXES), coords(MAXNAX), i,
+     *          iblc(MAXNAX), ictrpx, itrc(MAXNAX), ivaxnr, ivelpx, j,
+     *          jold, lIn, lOut, lTem, naxis, nchan, nprofs,
+     *          taxlen(MAXNAX), vaxnr, viraxlen(MAXNAX), vircsz(MAXNAX)
+      real      rdat(MAXDIM), fracshift, refpix, tdat(MAXDIM), vel,
+     *          work(MAXDIM)
+      double precision b(MAXDIM), c(MAXDIM), d(MAXDIM), seval, u, velpx,
+     *          x(MAXDIM), y(MAXDIM)
+      character inp*1024, line*80, opts(NOPTS)*8, outp*1024, tmpl*1024,
+     *          velaxis, version*72
 
-       integer      tinp, tout, ttpl
-       integer      nprofs, nchan
-       real         refpix
-       logical      doblank
+      external  len1, versan
+      integer   len1
+      character versan*72
 
-       call output(version)
-       call inputs(tinp, tout, ttpl, nprofs, nchan, refpix,doblank)
-       call vshift(tinp, tout, ttpl, nprofs, nchan, refpix,doblank)
-       call finish(tinp, tout, ttpl, version)
-
-       end
-
+      data      opts /'doblank '/
 c-----------------------------------------------------------------------
+      version = versan('specshift',
+     *                 '$Revision$',
+     *                 '$Date$')
 
-       subroutine inputs(tinp,tout,ttpl,nprofs,nchan,refpix,doblank)
+c     Get and check the inputs.
+      call keyini
 
-       include      'maxnax.h'
+      call keyf('in',  inp, ' ')
+      call assertl(inp.ne.' ', 'Input cube name is missing')
 
-       integer      tinp, tout, ttpl
-       integer      nprofs, nchan
+      call keyf('tin', tmpl, ' ')
+      call assertl(tmpl.ne.' ', 'Template image name is missing')
 
-       character*1024 inp, out, tpl
-       integer      MAXBOXES, NOPTS
-       parameter    (MAXBOXES = 1024)
-       parameter    (NOPTS = 1)
-       integer      boxes(MAXBOXES)
+      call keya('out', outp, ' ')
+      call assertl(outp.ne.' ', 'Output file name is missing')
 
-       integer      naxis
-       integer      axlen(MAXNAX), taxlen(MAXNAX)
-       integer      iblc(MAXNAX),  itrc(MAXNAX)
-       integer      viraxlen(MAXNAX), vircsz(MAXNAX)
+      call keyr('refpix',refpix,0.0)
 
-       character    velaxis, msg*132, opts(NOPTS)*8
-       integer      vaxnr, len1
-       real         refpix
-       data         opts/'doblank '/
-       logical      present(NOPTS), doblank
+      call boxinput('region', inp, boxes, MAXBOXES)
 
-       call keyini
+      call options('options',opts,present,NOPTS)
+      doblank = present(1)
 
-       call keyf('in',  inp, ' ')
-       call keyf('tin', tpl, ' ')
-       call keya('out', out, ' ')
-       call assertl(inp.ne.' ', 'Input cube name is missing')
-       call assertl(tpl.ne.' ', 'Template image name is missing')
-       call assertl(out.ne.' ', 'Output file name is missing')
-       call keyr('refpix',refpix,0.0)
-       naxis = MAXNAX
-       call xyzopen(tinp, inp, 'old', naxis, axlen)
+      call keyfin
 
-       call boxinput('region', inp, boxes, MAXBOXES)
-       call boxset(  boxes, naxis, axlen, ' ')
-       call boxinfo( boxes, naxis, iblc, itrc)
+c     Open the input image.
+      naxis = MAXNAX
+      call xyzopen(lIn, inp, 'old', naxis, axlen)
+      if (naxis.lt.3) then
+        call bug('f', 'Input image has fewer than three axes')
+      endif
 
-       call options('options',opts,present,NOPTS)
-       doblank = present(1)
+c     Set regions.
+      call boxset(boxes, naxis, axlen, ' ')
+      call boxinfo(boxes, naxis, iblc, itrc)
 
-       call keyfin
+c     Find the spectral axis.
+      velaxis = 'z'
+      call fndaxnum(lIn, 'freq', velaxis, vaxnr)
 
-c Set up the input and output cube
-       velaxis = 'z'
-       call fndaxnum(tinp, 'freq', velaxis, vaxnr)
+c     Set up for reading the spectral axis.
+      call xyzsetup(lIn, velaxis, iblc, itrc, viraxlen, vircsz)
+      nchan  = viraxlen(1)
+      nprofs = vircsz(naxis) / vircsz(1)
 
-       call xyzsetup(tinp, velaxis, iblc, itrc, viraxlen, vircsz)
-       nprofs = vircsz(naxis) / vircsz(1)
-       nchan     = viraxlen(1)
+c     Open the output image and set up for writing.
+      call xyzopen(lOut, outp, 'new', naxis, axlen)
+      call headcp(lIn, lOut, naxis, 0, iblc, itrc)
+      call xyzsetup(lOut, velaxis, iblc, itrc, viraxlen, vircsz)
 
-       call xyzopen(tout, out, 'new', naxis, axlen)
-       call headcp(tinp, tout, naxis, 0, iblc, itrc)
-       call xyzsetup(tout, velaxis, iblc, itrc, viraxlen, vircsz)
+c     Open the template image.
+      call xyopen(lTem, tmpl, 'old', maxnax, taxlen)
+      call rdhdi(lTem, 'naxis', naxis, 0)
+      if (naxis.gt.2 .and. taxlen(3).gt.1) then
+        call bug('w', tmpl(1:len1(tmpl))//' appears to have >2 axes')
+        call bug('w',
+     *    'Only the first plane of the 3rd axis will be used')
+      endif
+      call xysetpl(lTem, 1, 1)
 
-c Set up the input image
-       call xyopen(ttpl, tpl, 'old', maxnax, taxlen)
-       call rdhdi(ttpl, 'naxis', naxis, 0)
-       if (naxis.gt.2 .and. taxlen(3).gt.1) then
-         msg = tpl(1:len1(tpl))//' appears to have >2 axes'
-         call bug('w', msg)
-         msg = 'Only the first plane of the 3rd axis will be used'
-         call bug('w', msg)
-       endif
+c     Check image sizes for consistency.
+      if (axlen(1).ne.taxlen(1)) then
+        call bug('f', 'Unequal dimensions for input images on axis 1')
+      endif
+      if (axlen(2).ne.taxlen(2)) then
+        call bug('f', 'Unequal dimensions for input images on axis 2')
+      endif
 
-c Check image sizes for consistency
-       if (axlen(1).ne.taxlen(1)) then
-         msg = 'Unequal dimensions for input images on axis 1'
-         call bug('f', msg)
-       endif
-       if (axlen(2).ne.taxlen(2)) then
-         msg = 'Unequal dimensions for input images on axis 2'
-         call bug('f', msg)
-       endif
 
-       return
-       end
+c     Set up cube.
+      call coInit(lIn)
+      call coFindAx(lIn,'spectral',ivaxnr)
 
-c-----------------------------------------------------------------------
+      if (refpix.gt.0) then
+        ictrpx = nint(refpix)
+      else
+        ictrpx = nint(nchan/2.0)
+      endif
+      write(line,'(a,i4)') 'Shifting template velocity to pixel',ictrpx
+      call output(line)
 
-       subroutine vshift(tinp,tout,ttpl,nprofs,nchan,refpix,doblank)
+      do i = 1, nprofs
+c       Read the profile.
+        call xyzs2c(lIn, i, coords)
+        call xyzread(lIn, coords, rdat, mask, nchan)
 
-       integer       tinp, tout, ttpl
-       integer       nprofs, nchan
-       real          refpix, vel, fracshift
+c       Read the velocity from the image.
+        call xyread(lTem, coords(2), tdat)
+        call xyflgrd(lTem, coords(2), flags)
+        if (flags(coords(1))) then
+          vel = tdat(coords(1))
+          call coCvt1(lIn, ivaxnr, 'aw', dble(vel), 'ap', velpx)
+          ivelpx = int(velpx)
+          fracshift = velpx - ivelpx
+c         write(40,*) 'vel,velpx,fracshift: ',vel,velpx,fracshift
+        else
+          ivelpx = 0
+        endif
 
-       include       'maxdim.h'
-       include       'maxnax.h'
-       real          data(MAXDIM), work(MAXDIM), tdata(MAXDIM)
-       logical       mask(MAXDIM), workmsk(MAXDIM), flags(MAXDIM)
-       logical       doblank, outrange
-       integer       coords(MAXNAX)
-       double precision x(MAXDIM), y(MAXDIM), u
-       double precision b(MAXDIM), c(MAXDIM), d(MAXDIM)
+c       Shift if velocity is in range; otherwise do nothing.
+        if (ivelpx.gt.1 .and. ivelpx.lt.nchan) then
+          do j = 1, nchan + 1
+            outrange=.false.
+            jold = j + ivelpx - ictrpx
+            do while (jold.lt.1)
+              jold = jold + nchan
+              outrange=.true.
+            enddo
 
-       integer       i, j, jold, ictrpx, ivelpx, ivaxnr
-       double precision velpx, seval
-       character     line*80
+            do while (jold.gt.nchan)
+              jold = jold - nchan
+              outrange=.true.
+            enddo
 
-c Set up cube
-       call coInit(tinp)
-       call coFindAx(tinp,'spectral',ivaxnr)
-c Set up template
-       call xysetpl(ttpl, 1, 1)
+            work(j) = rdat(jold)
+            if (doblank .and. outrange) then
+              workmsk(j) = .false.
+            else
+               workmsk(j) = mask(jold)
+            endif
 
-       if (refpix.gt.0) then
-          ictrpx = nint(refpix)
-       else
-          ictrpx = nint(nchan/2.0)
-       endif
-       write(line,'(a,i4)') 'Shifting template velocity to pixel',ictrpx
-       call output(line)
+            x(j) = dble(j)
+            y(j) = dble(work(j))
+          enddo
 
-       do i = 1, nprofs
-c Read the profile
-          call xyzs2c(tinp, i, coords)
-          call xyzread(tinp, coords, data, mask, nchan)
-c Read the velocity from the image
-          call xyread(ttpl, coords(2), tdata)
-          call xyflgrd(ttpl, coords(2), flags)
-          if (flags(coords(1))) then
-             vel = tdata(coords(1))
-             call coCvt1(tinp,ivaxnr,'aw',dble(vel),'ap',velpx)
-             ivelpx = int(velpx)
-             fracshift = velpx - ivelpx
-c            write(40,*) 'vel,velpx,fracshift: ',vel,velpx,fracshift
-          else
-             ivelpx = 0
-          endif
-c Make the shift if velocity is in range; otherwise do nothing.
-          if (ivelpx.gt.1 .and. ivelpx.lt.nchan) then
-             do j = 1, nchan + 1
-                outrange=.false.
-                jold = j + ivelpx - ictrpx
-                do while (jold.lt.1)
-                   jold = jold + nchan
-                   outrange=.true.
-                enddo
-                do while (jold.gt.nchan)
-                   jold = jold - nchan
-                   outrange=.true.
-                enddo
-                work(j) = data(jold)
-                if (doblank .and. outrange) then
-                   workmsk(j) = .false.
-                else
-                   workmsk(j) = mask(jold)
-                endif
-                x(j) = dble(j)
-                y(j) = dble(work(j))
-             enddo
-             call spline(nchan+1, x, y, b, c, d)
-             do j = 1, nchan
-                u = x(j) + fracshift
-                work(j) = seval(nchan+1, u, x, y, b, c, d)
-             enddo
-          else
-             do j = 1, nchan
-                work(j) = data(j)
-                workmsk(j) = mask(j)
-             enddo
-          endif
-          call xyzprfwr(tout, i, work, workmsk, nchan)
-       enddo
+          call spline(nchan+1, x, y, b, c, d)
+          do j = 1, nchan
+            u = x(j) + fracshift
+            work(j) = seval(nchan+1, u, x, y, b, c, d)
+          enddo
+        else
+          do j = 1, nchan
+            work(j) = rdat(j)
+            workmsk(j) = mask(j)
+          enddo
+        endif
 
-       return
-       end
+        call xyzprfwr(lOut, i, work, workmsk, nchan)
+      enddo
 
-c-----------------------------------------------------------------------
+c     Write history.
+      call hisopen( lOut, 'append')
+      call hisinput(lOut, 'SPECSHIFT: '//version)
+      call hisclose(lOut)
 
-       subroutine finish(tinp, tout, ttpl, version)
+c     Finish up.
+      call xyzclose(lIn)
+      call xyzclose(lOut)
+      call xyclose(lTem)
 
-       integer       tinp, tout, ttpl
-       character*(*) version
-       character*80  line
-
-       call hisopen( tout, 'append')
-       line = 'SPECSHIFT: ' // version
-       call hisinput(tout, 'SPECSHIFT')
-       call hisclose(tout)
-       call xyzclose(tinp)
-       call xyzclose(tout)
-       call xyclose(ttpl)
-
-       return
-       end
+      end
