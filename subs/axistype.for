3b79e4a08202dc27dd19e1024b490c1451de50ce
Mark Calabretta <mcalabre@atnf.csiro.au>
2012-02-22 07:01:49 +0000

    Use coCtype to parse the coordinate type.

--- subs/axistype.for
+++ subs/axistype.for
@@ -3,14 +3,14 @@ c* Axistype - Find the axis label and plane value in user fiendly units
 c& mchw
 c: plotting
 c+
-      subroutine axisType(lIn,axis,plane,ctype,label,value,units)
+      subroutine axisType(lIn,axis,plane,ctype,label,value,valstr)
 
       integer   lIn, axis, plane
       character ctype*(*), label*13
       double precision value
-      character units*13
-c-----------------------------------------------------------------------
-c  Find the axis label and plane value in user friendly units.
+      character valstr*13
+c  ---------------------------------------------------------------------
+c  Find the axis label and plane value in user friendly string.
 c
 c  Input:
 c    lIn        The handle of the image.
@@ -21,54 +21,55 @@ c  Output:
 c    ctype      The official ctype for the input axis.
 c    label      A nice label for this axis.
 c    value      The value at the plane along this axis.
-c    units      User friendly units for this axis.
+c    valstr     value as a formatted string.
 c--
 c $Id$
 c-----------------------------------------------------------------------
       include 'mirconst.h'
 
-      integer   naxis
-      double precision cdelt, crpix, crval
-      character cax*2
+      integer   k, naxis
+      character algo*3, axtype*16, cax*2, units*8, wtype*16
 
-      external  angles, itoaf
+      external  angles, itoaf, len1
+      integer   len1
       character angles*13, itoaf*2
 c-----------------------------------------------------------------------
-c     Get ctype and value for input plane.
+c     Get coordinate type and value for input plane.
       cax = itoaf(axis)
       call rdhdi(lIn, 'naxis', naxis, 1)
       if (axis.le.naxis) then
         call coInit(lIn)
         call coCvt1(lIn, axis, 'ap', dble(plane), 'aw', value)
-        call coAxGet(lIn, axis, ctype, crpix, crval, cdelt)
+        call coGetA(lIn, 'ctype'//cax, ctype, ' ')
+        call coCtype(ctype, axtype, wtype, algo, units)
 
-c       Convert to user friendly label and units.
-        if (ctype(1:2).eq.'RA') then
-          label = '     RA      '
-          units = angles(value*DR2D/15d0)
+c       Convert to user friendly label and formatted value.
+        k = (13 - len1(wtype))/2 + 1
+        if (wtype.eq.'RA') then
+          label(k:) = wtype
+          valstr = angles(value*DR2D/15d0)
 
-        else if (ctype(1:3).eq.'DEC') then
-          label = '     DEC     '
-          units = angles(value*DR2D)
+        else if (units.eq.'rad') then
+          label(k:) = wtype
+          valstr = angles(value*DR2D)
 
-        else if (ctype(1:4).eq.'VRAD' .or. ctype(1:4).eq.'VOPT' .or.
-     *           ctype(1:4).eq.'VELO' .or. ctype(1:4).eq.'FELO') then
-          label = '  Velocity   '
-          write(units,'(g13.3)') value
+        else if (units.eq.'km/s') then
+          label(k:) = wtype
+          write(valstr,'(g13.3)') value
 
-        else if (ctype(1:4).eq.'FREQ') then
+        else if (units.eq.'GHz') then
           label = '  Frequency  '
-          write(units,'(g13.6)') value
+          write(valstr,'(g13.6)') value
 
         else
-          label = ctype
-          write(units,'(g13.6)') value
+          label(k:) = wtype(:13)
+          write(valstr,'(g13.6)') value
         endif
 
       else
-        value = 0d0
-        label = 'no axis '//cax
-        units = ' '
+        value  = 0d0
+        label  = 'no axis '//cax
+        valstr = ' '
       endif
 
       end
