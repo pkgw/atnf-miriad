3c9a9905181be3e555e23a9c9511df5d817afbb6
Mark Wieringa <Mark.Wieringa@csiro.au>
2012-06-14 05:22:24 +0000

    increase max open files

--- prog/uvsplit.h
+++ prog/uvsplit.h
@@ -5,7 +5,7 @@ c-----------------------------------------------------------------------
       include 'maxdim.h'
       integer MAXFILES, MAXOPEN
       parameter (MAXFILES = 8192)
-      parameter (MAXOPEN  = 6)
+      parameter (MAXOPEN  = 64)
 
       integer npol,nopen,nfiles,ifno(MAXFILES),indx(MAXFILES),nwins
       integer vCheck(MAXFILES),vCopy(MAXFILES),lOut(MAXFILES),lVis
