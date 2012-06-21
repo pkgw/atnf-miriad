176fb93ffb59880f7963049c03aed29c94085ef8
Mark Wieringa <Mark.Wieringa@csiro.au>
2012-06-21 00:45:18 +0000

    add carma's bugv_c

--- subs/bug.c
+++ subs/bug.c
@@ -9,20 +9,27 @@
 *                    to appease some compilers.
 *    rjs      9aug02 Support for "darwin" OS.
 *    rjs      3jul04 Use strerror routine.
+*    pjt      1jan05 bugv_c: finally, a real stdargs version!!!        
+*                    though cannot be exported to Fortran              
+*    pkgw    14dec11 Make errmsg_c public for use in uvio.c            
 *
 *  $Id$
 *===========================================================================*/
 
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 #include "miriad.h"
-#include "sysdep.h"
+/*#include "sysdep.h"*/
 
-private char *errmsg_c();
+char *errmsg_c();
 
 char *Name = NULL;
 int reentrant=0;
+
+#define MAXMSG 256
+static char msg[MAXMSG];      /* formatted message for bugv_c()         */
 
 /************************************************************************/
 
@@ -44,33 +51,6 @@ void buglabel_c(Const char *name)
   if(Name != NULL)free(Name);
   Name = malloc(strlen(name)+1);
   strcpy(Name,name);
-}
-
-/************************************************************************/
-
-void bugno_c(char s,int n)
-/** bugno -- Issue an error message, given a system error number.	*/
-/*& mjs									*/
-/*: error-handling							*/
-/*+ FORTRAN call sequence:
-      subroutine bugno(severity,errno)
-      character severity*1
-      integer errno
-
-  Output the error message associated with a particular error number.
-
-  Input:
-    severity	Error severity. Can be one of 'i', 'w', 'e' or 'f'
-		for "informational", "warning", "error", or "fatal"
-    errno	host error number.					*/
-/*--									*/
-/*----------------------------------------------------------------------*/
-{
-  if (n == -1) {
-    bug_c(s,"End of file detected");
-  } else {
-    bug_c(s,errmsg_c(n));
-  }
 }
 
 /************************************************************************/
@@ -115,11 +95,65 @@ void bug_c(char s,Const char *m)
     exit(1);
   }
 }
+/************************************************************************/
+void bugv_c(char s,Const char *m, ...)
+/** bugv_c -- Issue a dynamic error message, given by the caller.	*/
+/*& pjt									*/
+/*: error-handling							*/
+/*+ C call sequence:
+	bugv_c(severity,message,....)
+
+  Output the error message given by the caller, and abort if needed.
+  Note this routine has no counterpart in FORTRAN.
+
+  Input:
+    severity	Error severity character. 
+                Can be one of 'i', 'w', 'e' or 'f'
+		for "informational", "warning", "error", or "fatal"
+    message	The error message string, can contain %-printf style 
+                directives, as used by the following arguments.
+     ...         Optional argument, in the printf() style               */
+/*--									*/
+/*----------------------------------------------------------------------*/
+{
+  va_list ap;
+
+  va_start(ap,m);
+  vsnprintf(msg,MAXMSG,m,ap);
+  msg[MAXMSG-1] = '\0'; /* backstop */
+  va_end(ap);
+
+  bug_c(s, msg);
+}
 
 /************************************************************************/
 
-private char *errmsg_c(n)
-int n;
+void bugno_c(char s,int n)
+/** bugno -- Issue an error message, given a system error number.	*/
+/*& mjs									*/
+/*: error-handling							*/
+/*+ FORTRAN call sequence:
+      subroutine bugno(severity,errno)
+      character severity*1
+      integer errno
+
+  Output the error message associated with a particular error number.
+
+  Input:
+    severity	Error severity. Can be one of 'i', 'w', 'e' or 'f'
+		for "informational", "warning", "error", or "fatal"
+    errno	host error number.					*/
+/*--									*/
+/*----------------------------------------------------------------------*/
+{
+  if (n == -1) {
+    bug_c(s,"End of file detected");
+  } else {
+    bug_c(s,errmsg_c(n));
+  }
+}
+/************************************************************************/
+char *errmsg_c(int n)
 /*
   Return the error message associated with some error number.
 ------------------------------------------------------------------------*/
