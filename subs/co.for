966d7ab3df5cd321f8ddfa9f2c6d1eab34e91509
Mark Calabretta <mcalabre@atnf.csiro.au>
2012-08-29 03:03:25 +0000

    In coReinit, added some magic for WCSLIB to be able to handle simple linear celestial coordinates as a non-oblique CAR projection.  In coSetD, DEFS is false for lonpole and latpole if their value is set to 999.0, a magic value understood by WCSLIB as meaning "undefined".

--- subs/co.for
+++ subs/co.for
@@ -1304,10 +1304,10 @@ c     Parse parameterized keywords.
 
       else if (obj.eq.'lonpole') then
         status = celptd(cel(1,icrd), CEL_REF, value, 3)
-        defs(1,icrd) = .true.
+        defs(1,icrd) = (value.ne.999d0)
       else if (obj.eq.'latpole') then
         status = celptd(cel(1,icrd), CEL_REF, value, 4)
-        defs(2,icrd) = .true.
+        defs(2,icrd) = (value.ne.999d0)
       else if (obj.eq.'phi0') then
         status = celptd(cel(1,icrd), CEL_PHI0, value, 0)
         defs(3,icrd) = .true.
@@ -2727,6 +2727,13 @@ c             Convert GLS to SFL for WCSLIB.
               status = celptd(cel(1,icrd), CEL_PHI0,   0d0,  0)
               status = celptd(cel(1,icrd), CEL_THETA0, lat0, 0)
               status = prjptc(prj, PRJ_CODE, 'SFL', 0)
+
+            else if (pcode1.eq.' ') then
+c             Convert simple linear coordinates to CAR for WCSLIB.
+              status = celpti(cel(1,icrd), CEL_OFFSET, 1, 0)
+              status = celptd(cel(1,icrd), CEL_PHI0,   0d0, 0)
+              status = celptd(cel(1,icrd), CEL_THETA0, lat0, 0)
+              status = prjptc(prj, PRJ_CODE, 'CAR', 0)
 
             else
               status = prjptc(prj, PRJ_CODE, pcode1, 0)
