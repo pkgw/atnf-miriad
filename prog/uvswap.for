59c6499b89f6eaa87eb5d3d87f61bb910c5013d3
Bob Sault <rsault@nrao.edu>
2012-09-04 17:35:05 +0000

    Bring across BIMA version

--- prog/uvswap.for
+++ prog/uvswap.for
@@ -1,12 +1,13 @@
 	program uvswap
 	implicit none
 
-c= uvswap - Relabel correlations RL as LR and LR as RL
+c= uvswap - Relabel polarizations in uvdata.
 c& nebk
 c: uv analysis
 c+
 c	UVSWAP relabels RL correlations as LR and LR correlations as RL.
-c	RR and LL are left unchanged.  The data are copied in their 
+c	XY are relabeled YX and YX correlations as XY.
+c	RR, LL, XX and YY  are left unchanged.  The data are copied in their 
 c	raw form only, gain, polarization and bandpass tables are 
 c	neither applied nor copied.
 c
@@ -14,21 +15,26 @@ c@ vis
 c	Input visibility data file. No default
 c@ out
 c	Output visibility data file name. No default. 
+c@ options
+c      xyswap  swap X and Y pols.
+c   XX to YY, YY to XX, XY to YX and YX to XY.
 c--
 c
 c  History:
 c    nebk 28sep92 Original version cloned from UVMODEL
+c    mchw 10apr09 Added XY and YX swap.
+c    mchw 16apr09 Added options=xywap
 c
 c------------------------------------------------------------------------
       include 'maxdim.h'
       character version*(*)
-      parameter(version='version 28-Sep-92')
+      parameter(version='version 16-Apr-2009')
 c
       character vis*64, out*64
       integer nread, npols, pol, tvis, tout
       double precision preamble(4)
       complex data(maxchan)
-      logical flags(maxchan)
+      logical flags(maxchan), doxyswap
 c------------------------------------------------------------------------
 c
 c Get the input parameters
@@ -37,6 +43,7 @@ c
       call keyini
       call keya ('vis', vis, ' ')
       call keya ('out', out, ' ')
+        call GetOpt(doxyswap)
       call keyfin
 c
 c Check the input parameters
@@ -59,11 +66,33 @@ c
         call uvgetvri (tvis, 'pol', pol, 1)
 c 
         call varcopy (tvis, tout)
+c
+c swap XY YX
+c
+        if (pol.eq.-7) then
+          pol = -8
+        else if (pol.eq.-8) then
+          pol = -7
+        endif 
+c
+c swap RL LR
+c
         if (pol.eq.-3) then
           pol = -4
         else if (pol.eq.-4) then
           pol = -3
-        end if 
+        endif 
+c
+c swap XX YY
+c
+       if(doxyswap) then
+         if(pol.eq.-6)then
+           pol = -5
+         else if(pol.eq.-5)then
+           pol = -6
+         endif
+       endif
+c
         call uvputvri (tout, 'npol', npols, 1)
         call uvputvri (tout, 'pol', pol, 1)
         call uvwrite (tout, preamble, data, flags, nread)
@@ -83,3 +112,24 @@ c
       call uvclose (tout)
 c
       end
+c********1*********2*********3*********4*********5*********6*********7*c 
+        subroutine GetOpt(doxyswap)
+        implicit none
+        logical doxyswap
+c
+c  Get extra processing options.
+c
+c  Output:
+c    doxyswap    xyswap  swap X and Y pols.
+c----------------------------------------------------------------------- 
+        integer nopt 
+        parameter(nopt=1) 
+        logical present(nopt) 
+        character opts(nopt)*9 
+c 
+        data opts/'xyswap   '/
+c    
+        call options('options',opts,present,nopt) 
+        doxyswap    = present(1) 
+        end
+c********1*********2*********3*********4*********5*********6*********7*c 
