0d2d5eb214cfd02264107a76f68a2a9012ff5da8
Mark Wieringa <Mark.Wieringa@csiro.au>
2013-09-26 04:25:36 +0000

    increase run buffer size

--- prog/maths.h
+++ prog/maths.h
@@ -8,16 +8,16 @@ c
 	INCLUDE 'maxnax.h'
 	INTEGER MAXFILES,MAXRUNS,XVAL,YVAL,ZVAL
 
-	PARAMETER(MAXFILES=16,MAXRUNS=80000)
+	PARAMETER(MAXFILES=16,MAXRUNS=MAXBUF)
 	PARAMETER(XVAL=MAXFILES+1,YVAL=MAXFILES+2,ZVAL=MAXFILES+3)
 c
 	INTEGER   nfiles,nsize(MAXNAX),naxis,Plane(MAXNAX)
-	INTEGER   lIn(MAXFILES),Offset(MAXFILES+1)
-	INTEGER   runs(3,MAXRUNS),nruns,blc(MAXNAX),trc(MAXNAX)
+	INTEGER   lIn(MAXFILES),Offset(MAXFILES+1),naxes(MAXFILES)
+	INTEGER   runs(3,MAXRUNS),nruns,blc(MAXNAX),trc(MAXNAX),ref
 	REAL      range(2,MAXNAX)
-	LOGICAL   xused,yused,zused
+	LOGICAL   xused,yused,zused,grow
 	CHARACTER names*256
-	COMMON/mathcom/nfiles,nsize,naxis,Plane,lIn,Offset,Runs,nRuns,
-     *		blc,trc,Range,Xused,Yused,Zused
+	COMMON/mathcom/nfiles,nsize,naxes,naxis,Plane,ref,lIn,Offset,
+     *		Runs,nRuns,blc,trc,Range,Xused,Yused,Zused,grow
 	COMMON/mathcomc/names
 
