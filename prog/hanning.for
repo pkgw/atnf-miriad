140e676da30b40f7100bd4a612509c03f66669a0
Mark Wieringa <Mark.Wieringa@csiro.au>
2016-09-14 03:44:47 +0000

    add warning about large cubes

--- prog/hanning.for
+++ prog/hanning.for
@@ -8,6 +8,9 @@ c       Hanning does a Hann or boxcar smooth on the spectral axis of
 c       a Miriad image cube.  It determines the axis from the header,
 c       or else assumes that it is the z-axis.  Masked pixels are 
 c       zeroed before smoothing.
+c       For very large cubes hanning smoothing the 3rd axis can be
+c       too slow, it may be much quicker to use reorder to put the 
+c       spectral axis first, then run hanning, and then reorder back.
 c
 c@ in
 c        The input image.  No default.
